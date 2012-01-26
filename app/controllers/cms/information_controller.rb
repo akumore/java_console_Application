c92652e1d26f02dbc1eb900b16c53ab39897952c
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-01-26 17:49:30 +0100

    add edit- and update action

--- app/controllers/cms/information_controller.rb
+++ app/controllers/cms/information_controller.rb
@@ -1,47 +1,30 @@
 class Cms::InformationController < Cms::SecuredController
 
+  respond_to :html, :json
+
   def new
     @real_estate = RealEstate.find params[:real_estate_id]
     @information = Information.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @information }
-    end
   end
 
-  #def edit
-  #  @information = Information.find(params[:id])
-  #end
+  def edit
+    @real_estate = RealEstate.find params[:real_estate_id]
+    @information = @real_estate.information
+  end
 
-  #def create
-  #  @information = Information.new(params[:information])
-  #
-  #  respond_to do |format|
-  #    if @information.save
-  #      format.html { redirect_to @information, notice: 'Information was successfully created.' }
-  #      format.json { render json: @information, status: :created, location: @information }
-  #    else
-  #      format.html { render action: "new" }
-  #      format.json { render json: @information.errors, status: :unprocessable_entity }
-  #    end
-  #  end
-  #end
+  def create
+    @real_estate = RealEstate.find params[:real_estate_id]
+    @information = Information.create! params[:information].merge(:real_estate=>@real_estate)
 
-  # PUT /information/1
-  # PUT /information/1.json
-  #def update
-  #  @information = Information.find(params[:id])
-  #
-  #  respond_to do |format|
-  #    if @information.update_attributes(params[:information])
-  #      format.html { redirect_to @information, notice: 'Information was successfully updated.' }
-  #      format.json { head :ok }
-  #    else
-  #      format.html { render action: "edit" }
-  #      format.json { render json: @information.errors, status: :unprocessable_entity }
-  #    end
-  #  end
-  #end
+    respond_with @information, :location=> edit_cms_real_estate_information_path(@real_estate)
+  end
+
+  def update
+    @real_estate = RealEstate.find params[:real_estate_id]
+    @information = @real_estate.information
+
+    @information.update_attributes params[:information]
+    respond_with @information, :location=> edit_cms_real_estate_information_path(@real_estate)
+  end
 
 end
