ac63a5ec3037837724976c1c2f4d3a7460d498fb
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-01-27 14:01:31 +0100

    enable validations

--- app/models/information.rb
+++ app/models/information.rb
@@ -31,6 +31,6 @@ class Information
   field :is_developed, :type=>Boolean
   field :is_under_building_laws, :type=>Boolean
 
-  #validates_size_of :maximal_floor_loading, :freight_elevator_carrying_capacity, :number_of_restrooms, :minimum=>0, :allow_blank=>true
+  validates_size_of :maximal_floor_loading, :freight_elevator_carrying_capacity, :number_of_restrooms, :minimum=>0, :allow_blank=>true
 
 end
