b3784cc2f10759a5a2ae5266c06ceb6311df6181
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-01-27 11:01:06 +0100

    remove obsolete comments

--- app/models/information.rb
+++ app/models/information.rb
@@ -7,28 +7,28 @@ class Information
   field :available_from, :type => Date
   field :display_estimated_available_from, :type=>String
   field :has_outlook, :type=>Boolean
-  field :has_fireplace, :type=>Boolean #Cheminée
-  field :has_elevator, :type=>Boolean #Lift
+  field :has_fireplace, :type=>Boolean
+  field :has_elevator, :type=>Boolean
   field :has_isdn, :type=>Boolean
-  field :is_wheelchair_accessible, :type=>Boolean #Barrierefrei
+  field :is_wheelchair_accessible, :type=>Boolean
   field :is_child_friendly, :type=>Boolean
   field :has_balcony, :type=>Boolean
-  field :has_raised_ground_floor, :type=>Boolean #Hochparterre
-  field :is_new_building, :type=>Boolean #Neubau
-  field :is_old_building, :type=>Boolean #Altbau
-  field :has_swimming_pool, :type=>Boolean #Swimmingpool
-  field :is_minergie_style, :type=>Boolean #Minergie Bauweise
-  field :is_minergie_certified, :type=>Boolean #Minergie zertifiziert
-  field :maximal_floor_loading, :type=>Integer #Maximale Bodenbelastung in KG
-  field :freight_elevator_carrying_capacity, :type=>Integer #carrying_capacity_elevator	Max Gewicht Warenlift
-  field :has_ramp, :type=>Boolean #Anfahrrampe
-  field :has_lifting_platform, :type=>Boolean #Hebebühne
-  field :has_railway_terminal, :type=>Boolean #Bahnanschluss
-  field :number_of_restrooms, :type=>Integer  #TODO BATH ROOMS?
-  field :has_water_supply, :type=>Boolean #Wasseranschluss
-  field :has_sewage_supply, :type=>Boolean #Abwasseranschluss
-  field :is_developed, :type=>Boolean #Bauland erschlossen
-  field :is_under_building_laws, :type=>Boolean #im Baurecht
+  field :has_raised_ground_floor, :type=>Boolean
+  field :is_new_building, :type=>Boolean
+  field :is_old_building, :type=>Boolean
+  field :has_swimming_pool, :type=>Boolean
+  field :is_minergie_style, :type=>Boolean
+  field :is_minergie_certified, :type=>Boolean
+  field :maximal_floor_loading, :type=>Integer
+  field :freight_elevator_carrying_capacity, :type=>Integer
+  field :has_ramp, :type=>Boolean
+  field :has_lifting_platform, :type=>Boolean
+  field :has_railway_terminal, :type=>Boolean
+  field :number_of_restrooms, :type=>Integer
+  field :has_water_supply, :type=>Boolean
+  field :has_sewage_supply, :type=>Boolean
+  field :is_developed, :type=>Boolean
+  field :is_under_building_laws, :type=>Boolean
 
   validates_size_of :maximal_floor_loading, :freight_elevator_carrying_capacity, :number_of_restrooms, :minimum=>0, :allow_blank=>true
 
