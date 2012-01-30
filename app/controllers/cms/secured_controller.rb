779a052e99ed95b8bb45952862305424eb43a005
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-01-30 16:05:30 +0100

    retry logger

--- app/controllers/cms/secured_controller.rb
+++ app/controllers/cms/secured_controller.rb
@@ -1,6 +1,6 @@
 class Cms::SecuredController < ApplicationController
-  before_filter :authenticate_user!
   before_filter :print_request_format
+  before_filter :authenticate_user!
   layout 'cms/application'
   respond_to :html
 
