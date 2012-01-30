faafb9f92b856bf17b1f91cd1b391789e8785830
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-01-30 17:52:35 +0100

    remove log message from secured controller

--- app/controllers/cms/secured_controller.rb
+++ app/controllers/cms/secured_controller.rb
@@ -1,10 +1,5 @@
 class Cms::SecuredController < ApplicationController
-  before_filter :print_request_format
   before_filter :authenticate_user!
   layout 'cms/application'
   respond_to :html
-
-  def print_request_format
-    logger.warn request.formats.inspect.to_s if logger
-  end
 end
