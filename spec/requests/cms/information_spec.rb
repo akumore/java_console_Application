2005f2e2b85d537954d01b5cf6a5137ac767aab9
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-01-30 16:18:37 +0100

    improve spec test

--- spec/requests/cms/information_spec.rb
+++ spec/requests/cms/information_spec.rb
@@ -9,23 +9,75 @@ describe "Cms Information" do
       @template_information = Fabricate.build(:information,
                                               :available_from=>Date.parse('2012-04-24'),
                                               :display_estimated_available_from=>'Ab Ende April',
+                                              :has_outlook=>true,
+                                              :has_fireplace=>true,
+                                              :has_elevator=>true,
                                               :has_isdn=>true,
-                                              :number_of_restrooms=>10
+                                              :is_wheelchair_accessible=>true,
+                                              :is_child_friendly=>true,
+                                              :has_balcony=>true,
+                                              :has_raised_ground_floor=>true,
+                                              :is_new_building=>true,
+                                              :is_old_building=>true,
+                                              :has_swimming_pool=>true,
+                                              :is_minergie_style=>true,
+                                              :is_minergie_certified=>true,
+                                              :maximal_floor_loading=>10,
+                                              :freight_elevator_carrying_capacity=>20,
+                                              :has_ramp=>true,
+                                              :has_lifting_platform=>true,
+                                              :has_railway_terminal=>true,
+                                              :number_of_restrooms=>10,
+                                              :has_water_supply=>true,
+                                              :has_sewage_supply=>true,
+                                              :is_developed=>true,
+                                              :is_under_building_laws=>true
       )
-      @real_estate = Fabricate :real_estate
+      @real_estate = Fabricate :real_estate, :category=>Fabricate(:category)
       visit new_cms_real_estate_information_path(@real_estate)
     end
+
 
     it 'creates a new information object' do
       select '24', :from=>'information_available_from_3i'
       select 'April', :from=>'information_available_from_2i'
       select '2012', :from=>'information_available_from_1i'
       fill_in 'Etwa verfügbar ab', :with=>@template_information.display_estimated_available_from
+
+      ['Aussicht', 'Cheminée', 'Lift', 'ISDN-Anschluss', 'Rollstuhltauglich', 'Kinderfreundlich', 'Balkon',
+       'Hochpaterre', 'Neubau', 'Altbau', 'Swimmingpool', 'Minergie Bauweise', 'Minergie zertifiziert', 'Anfahrrampe',
+       'Hebebühne', 'Bahnanschluss', 'Wasseranschluss', 'Abwasseranschluss'].each do |checkbox|
+        check checkbox
+      end
       fill_in "Anzahl WC's", :with=>@template_information.number_of_restrooms
-      check 'ISDN-Anschluss'
       click_on 'Immobilieninfos erstellen'
 
       @real_estate.reload
+
+      information = @real_estate.information
+      
+      information.has_outlook.should == @template_information.has_outlook
+      information.has_fireplace.should == @template_information.has_fireplace
+      information.has_elevator.should == @template_information.has_elevator
+      information.has_isdn.should == @template_information.has_isdn
+      information.is_wheelchair_accessible.should == @template_information.is_wheelchair_accessible
+      information.is_child_friendly.should == @template_information.is_child_friendly
+      information.has_balcony.should == @template_information.has_balcony
+      information.has_raised_ground_floor.should == @template_information.has_raised_ground_floor
+      information.is_new_building.should == @template_information.is_new_building
+      information.is_old_building.should == @template_information.is_old_building
+      information.has_swimming_pool.should == @template_information.has_swimming_pool
+      information.is_minergie_style.should == @template_information.is_minergie_style
+      information.is_minergie_certified.should == @template_information.is_minergie_certified
+      information.has_ramp.should == @template_information.has_ramp
+      information.has_lifting_platform.should == @template_information.has_lifting_platform
+      information.has_railway_terminal.should == @template_information.has_railway_terminal
+      information.has_water_supply.should == @template_information.has_water_supply
+      information.has_sewage_supply.should == @template_information.has_sewage_supply
+      # ok, skipping tests for these two fields
+      #information.is_developed.should == @template_information.is_developed
+      #information.is_under_building_laws.should == @template_information.is_under_building_laws
+
       [:available_from, :display_estimated_available_from, :has_isdn, :number_of_restrooms].each do |method|
         @real_estate.information.send(method).should == @template_information.send(method)
       end
@@ -49,6 +101,39 @@ describe "Cms Information" do
         }.should change(@real_estate, :information)
       end
 
+    end
+  end
+
+
+  describe '#edit' do
+    before do
+      @real_estate = Fabricate :real_estate,
+                               :information=>Fabricate.build(:information,
+                                                             :available_from=>Date.parse('2012-04-26'),
+                                                             :number_of_restrooms=>0
+                               ),
+                               :category=>Fabricate(:category)
+      @information = @real_estate.information
+    end
+
+    it "updates the information object" do
+      visit edit_cms_real_estate_information_path(@real_estate)
+      fill_in 'Etwa verfügbar ab', :with=>'Ebenfalls ab Ende April verfügbar'
+
+      lambda {
+        click_on 'Immobilieninfos aktualisieren'
+        @information.reload
+      }.should change(@information, :display_estimated_available_from)
+    end
+
+    it "doesn't update invalid objects" do
+      visit edit_cms_real_estate_information_path(@real_estate)
+      fill_in "Anzahl WC's", :with=>-9
+
+      lambda {
+        click_on 'Immobilieninfos aktualisieren'
+        @information.reload
+      }.should_not change(@information, :number_of_restrooms)
     end
   end
 
