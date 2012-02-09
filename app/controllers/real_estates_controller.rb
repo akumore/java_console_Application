839237c64ee2d90a3bad67233e5463ec4df85a66
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-02-09 13:06:08 +0100

    add prev/next link for search result browsing

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -1,5 +1,5 @@
 class RealEstatesController < ApplicationController
-  
+
   respond_to :html
 
   before_filter :set_search_filter
@@ -10,14 +10,18 @@ class RealEstatesController < ApplicationController
 
   def show
     @real_estate = RealEstateDecorator.find(params[:id])
+    real_estates = get_filtered_real_estates(@search_filter).map(&:id)
+    @prev_real_estate = real_estates[real_estates.index(@real_estate.id) - 1] rescue real_estates.last
+    @next_real_estate = real_estates[real_estates.index(@real_estate.id) + 1] rescue real_estates.first
   end
 
   private
+
   def set_search_filter
     @search_filter = SearchFilter.new :offer=>params[:offer], :utilization=>params[:utilization]
   end
 
   def get_filtered_real_estates(search_filter)
-    RealEstate.where(search_filter.to_h).all
+    RealEstate.where(search_filter.to_h)
   end
 end
