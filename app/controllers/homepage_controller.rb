77eeb1179713933c8a8373378db0ae406f0d334e
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-02-23 18:29:05 +0100

    get reference projects on home page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,9 @@
 class HomepageController < ApplicationController
+
   def index
-    
+    @projects_for_rent = RealEstate.reference_projects.where(:offer=>RealEstate::OFFER_FOR_RENT).all
+    @projects_for_sale = RealEstate.reference_projects.where(:offer=>RealEstate::OFFER_FOR_SALE).all
+    @projects_for_build = [] #TODO What kind of real estate? Is this something like 'OFFER_FOR_BUILD?'
   end
+
 end
