996e61af37633875dbe4cc102cf55d2bd40c1428
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-02-27 18:16:38 +0100

    use decorator for rendering reference project caption

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,9 @@
 class HomepageController < ApplicationController
 
   def index
-    @projects_for_rent = RealEstate.reference_projects.where(:offer=>RealEstate::OFFER_FOR_RENT).all
-    @projects_for_sale = RealEstate.reference_projects.where(:offer=>RealEstate::OFFER_FOR_SALE).all
-    @projects_for_build = [] #TODO What kind of real estate? Is this something like 'OFFER_FOR_BUILD?'
+    @projects_for_rent = RealEstateDecorator.decorate RealEstate.reference_projects.where(:offer=>RealEstate::OFFER_FOR_RENT).all
+    @projects_for_sale = RealEstateDecorator.decorate RealEstate.reference_projects.where(:offer=>RealEstate::OFFER_FOR_SALE).all
+    @projects_for_build = RealEstateDecorator.decorate [] #TODO What kind of real estate? Is this something like 'OFFER_FOR_BUILD?'
   end
 
 end
