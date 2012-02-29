cd801ccb054ba77fa975bda7718153051bb3df39
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-02-29 13:34:14 +0100

    add geocoding to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,9 @@ class Address
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  include Geocoder::Model::Mongoid
+  geocoded_by :address, :coordinates => :location
+
   CANTONS = %w(ag ar ai bl bs be fr ge gl gr ju lu ne nw ow sh sz so sg ti tg ur vd vs zg zh)
 
   embedded_in :real_estate
@@ -12,5 +15,23 @@ class Address
   field :street_number, :type => String
   field :zip, :type => String
   field :canton, :type => String
+  field :country, :type=>String, :defaults=>"Schweiz"
   field :link_url, :type => String
+  field :location, :type => Array #Keep in mind coordinates are stored in long, lat order internally!! Use to_coordinates always.
+  #index [[ :location, Mongo::GEO2D ]] TODO Do we need this?
+
+
+  after_validation :geocode, :if=>:address_changed?
+
+
+  def address
+    [street, zip, city, canton, country].compact.join(', ')
+  end
+
+  def address_changed?
+    [:street, :zip, :city, :canton, :country].inject(false) do |res, attr|
+      res || changed.include?(attr.to_s)
+    end
+  end
+
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,55 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @address = Fabricate.build(:address, :zip => nil, :city => 'Herrnhut', :street => 'Christian-David-Straße 12', :canton => nil, :country => 'Deutschland')
+  end
+
+  it "collects address fields" do
+    @address.address.should == "Christian-David-Straße 12, Herrnhut, Deutschland"
+  end
+
+
+  describe "detection of changes in geo-coding related fields" do
+    before do
+      Fabricate(:real_estate, :address => @address)
+    end
+
+    [:street, :zip, :city, :canton, :country].each do |attr|
+      it "detects changed attribute #{attr}" do
+        @address.send("#{attr}=", "this value has changed")
+        @address.address_changed?.should be_true
+      end
+    end
+
+    it "doesn't detect changes in other fields" do
+      @address.link_url = "test link"
+      @address.address_changed?.should be_false
+    end
+  end
+
+
+  it 'geocodes itself on create' do
+    mock_geocoding!(:coordinates => [8, 15])
+    Fabricate :real_estate, :address => @address
+    @address.to_coordinates.should == [8, 15]
+  end
+
+  it 'geocodes itself when address is changing' do
+    Fabricate :real_estate, :address => @address
+    mock_geocoding!(:coordinates => [5, 6])
+    @address.update_attributes :street => 'A new street'
+
+    @address.to_coordinates.should == [5, 6]
+  end
+
+  it "doesn't geocode on update without address changing" do
+    Fabricate :real_estate, :address => @address
+    mock_geocoding!(:coordinates => [5, 6])
+    @address.update_attributes :link_url => "www.heise.de"
+
+    @address.to_coordinates.should_not == [5, 6]
+  end
 end
