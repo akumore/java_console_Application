e49e9bf2a5317ccf6a9c235282d94f2c0a894d99
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-02 12:56:47 +0100

    add factory for creating published real estates

--- spec/fabricators/real_estate_fabricator.rb
+++ spec/fabricators/real_estate_fabricator.rb
@@ -1,13 +1,17 @@
 Fabricator(:real_estate) do
-	state RealEstate::STATE_EDITING
+  state RealEstate::STATE_EDITING
   utilization RealEstate::UTILIZATION_PRIVATE
   offer RealEstate::OFFER_FOR_RENT
-  channels { [RealEstate::CHANNELS.first] }
-  title 'A fine real estate property'
+  channels { [RealEstate::WEBSITE_CHANNEL] }
+  title { "A fine real estate property #{Fabricate.sequence}" }
   property_name 'Garden City'
   description 'Some real estate description...'
   short_description 'Some short real estate description...'
   keywords 'Real, Estate, Stuff'
   is_first_marketing true
   utilization_description 'Commercial, Restaurant'
+end
+
+Fabricator(:published_real_estate, :from => :real_estate) do
+  state RealEstate::STATE_PUBLISHED
 end
