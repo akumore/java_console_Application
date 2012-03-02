4542462a6d3818f08663afdf37d3983ac0e757b9
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-02 15:26:27 +0100

    add publishing of real estates to cms

--- app/controllers/cms/secured_controller.rb
+++ app/controllers/cms/secured_controller.rb
@@ -2,4 +2,19 @@ class Cms::SecuredController < ApplicationController
   before_filter :authenticate_user!
   layout 'cms/application'
   respond_to :html
+
+  def next_state(real_estate)
+    return real_estate.state_name unless real_estate.valid?
+    case real_estate.state_name
+      when :editing
+        current_user.is_admin? ? :published : :in_review
+      when :in_review
+        current_user.is_admin? ? :published : :in_review
+      else
+        :editing
+    end
+  end
+
+  helper_method :next_state
+
 end
