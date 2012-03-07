f450566fe40153cc3cb063ccf7e1fd58827038cc
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-03-07 17:52:47 +0100

    add wizard spec for information controller

--- spec/controllers/cms/real_estates_wizard_spec.rb
+++ spec/controllers/cms/real_estates_wizard_spec.rb
@@ -54,7 +54,7 @@ describe 'Real Estate Wizard' do
         response.should redirect_to(new_cms_real_estate_information_path(mock))
       end
 
-      it 'redirects to the new information tab with an existing information' do
+      it 'redirects to the edit information tab with an existing information' do
         mock = real_estate
         mock.stub!(:information).and_return(mock_model(Information))
 
@@ -78,7 +78,7 @@ describe 'Real Estate Wizard' do
         response.should redirect_to(new_cms_real_estate_information_path(mock))
       end
 
-      it 'redirects to the new information tab with an existing information' do
+      it 'redirects to the edit information tab with an existing information' do
         mock = real_estate
         mock.stub!(:information).and_return(mock_model(Information))
         mock.stub!(:address).and_return(mock_model(Address, :update_attributes => true))
@@ -90,6 +90,54 @@ describe 'Real Estate Wizard' do
         response.should redirect_to(edit_cms_real_estate_information_path(mock))
       end
     end
+  end
 
+  describe Cms::InformationController do
+    describe '#create' do
+      it 'redirects to the new pricing tab without an existing pricing' do
+        mock = real_estate
+        mock.stub!(:pricing).and_return(nil)
+
+        RealEstate.stub!(:find).and_return(mock)
+        Information.stub!(:new).and_return(mock_model(Information, :save => true, :real_estate= => nil))
+
+        post :create, :real_estate_id => mock.id
+        response.should redirect_to(new_cms_real_estate_pricing_path(mock))
+      end
+
+      it 'redirects to the edit pricing tab with an existing pricing' do
+        mock = real_estate
+        mock.stub!(:pricing).and_return(mock_model(Pricing))
+
+        RealEstate.stub!(:find).and_return(mock)
+
+        post :create, :real_estate_id => mock.id
+        response.should redirect_to(edit_cms_real_estate_pricing_path(mock))
+      end
+    end
+
+    describe '#update' do
+      it 'redirects to the new information tab without an existing information' do
+        mock = real_estate
+        mock.stub!(:pricing).and_return(nil)
+        mock.stub!(:information).and_return(mock_model(Information, :update_attributes => true))
+
+        RealEstate.stub!(:find).and_return(mock)
+
+        post :update, :real_estate_id => mock.id
+        response.should redirect_to(new_cms_real_estate_pricing_path(mock))
+      end
+
+      it 'redirects to the new information tab with an existing information' do
+        mock = real_estate
+        mock.stub!(:pricing).and_return(mock_model(Pricing))
+        mock.stub!(:information).and_return(mock_model(Information, :update_attributes => true))
+
+        RealEstate.stub!(:find).and_return(mock)
+
+        post :update, :real_estate_id => mock.id
+        response.should redirect_to(edit_cms_real_estate_pricing_path(mock))
+      end
+    end
   end
 end
