f33f7a186aca5a4106f73ec79e784e58e4ad606c
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-07 12:16:53 +0100

    add ability check to cms information controller

--- app/controllers/cms/information_controller.rb
+++ app/controllers/cms/information_controller.rb
@@ -1,6 +1,13 @@
 class Cms::InformationController < Cms::SecuredController
   include EmbeddedInRealEstate
 
+  respond_to :html, :json
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to cms_real_estate_information_path(@real_estate), :alert => exception.message
+  end
+
+
   def new
     @information = Information.new
     @information.real_estate = @real_estate
@@ -8,6 +15,7 @@ class Cms::InformationController < Cms::SecuredController
 
   def edit
     @information = @real_estate.information
+    authorize! :update,  @real_estate
   end
 
   def create
@@ -23,6 +31,7 @@ class Cms::InformationController < Cms::SecuredController
 
   def update
     @information = @real_estate.information
+    authorize! :update,  @real_estate
 
     if @information.update_attributes(params[:information])
       redirect_to_step('pricing')
@@ -30,4 +39,10 @@ class Cms::InformationController < Cms::SecuredController
       render 'edit'
     end    
   end
+
+  def show
+    @real_estate = RealEstate.find params[:real_estate_id]
+    @information = @real_estate.information
+  end
+  
 end
