cb7f9a9c416c4b59141200e26f2d1a2501bb7457
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-03-07 15:24:31 +0100

    simplify cms/description controller

--- app/controllers/cms/descriptions_controller.rb
+++ app/controllers/cms/descriptions_controller.rb
@@ -3,43 +3,32 @@ class Cms::DescriptionsController < Cms::SecuredController
 
   def new
     @description = Description.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @description }
-    end
+    respond_with @description
   end
 
   def edit
     @description = @real_estate.descriptions
+    respond_with @description
   end
 
   def create
     @description = Description.new(params[:description])
     @description.real_estate = @real_estate
 
-    respond_to do |format|
-      if @description.save
-        format.html { redirect_to edit_cms_real_estate_description_path(@real_estate) }
-        format.json { render :json => @description, :status => :created, :location => @description }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @description.errors, :status => :unprocessable_entity }
-      end
+    if @description.save
+      redirect_to edit_cms_real_estate_description_path(@real_estate)
+    else
+      render 'new'
     end
   end
 
   def update
     @description = @real_estate.descriptions
 
-    respond_to do |format|
-      if @description.update_attributes(params[:description])
-        format.html { redirect_to edit_cms_real_estate_description_path(@real_estate) }
-        format.json { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @description.errors, :status => :unprocessable_entity }
-      end
+    if @description.update_attributes(params[:description])
+      redirect_to edit_cms_real_estate_description_path(@real_estate)
+    else
+      render 'edit'
     end
   end
 end
