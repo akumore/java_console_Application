9410cd5b9f55482c4b3f882c4110a96841756dc7
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-08 11:40:59 +0100

    add show template for description model, add access control

--- app/controllers/cms/descriptions_controller.rb
+++ app/controllers/cms/descriptions_controller.rb
@@ -1,6 +1,11 @@
 class Cms::DescriptionsController < Cms::SecuredController
   include EmbeddedInRealEstate
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to cms_real_estate_description_path(@real_estate), :alert => exception.message
+  end
+
+
   def new
     @description = Description.new
     respond_with @description
@@ -25,6 +30,7 @@ class Cms::DescriptionsController < Cms::SecuredController
 
   def update
     @description = @real_estate.descriptions
+    authorize! :update, @real_estate
 
     if @description.update_attributes(params[:description])
       flash[:success] = success_message_for(controller_name)
@@ -33,4 +39,9 @@ class Cms::DescriptionsController < Cms::SecuredController
       render 'edit'
     end
   end
+
+  def show
+    @description = @real_estate.descriptions
+  end
+
 end
