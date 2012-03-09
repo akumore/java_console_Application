690935105d404cdba4d45f0be62f9777896e2693
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-09 11:58:09 +0100

    refactore authentication and object loading

--- app/controllers/cms/addresses_controller.rb
+++ app/controllers/cms/addresses_controller.rb
@@ -1,8 +1,6 @@
 class Cms::AddressesController < Cms::SecuredController
   include EmbeddedInRealEstate
 
-  authorize_resource :only => [:edit, :update], :through=>:real_estate
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to cms_real_estate_address_path(@real_estate), :alert => exception.message
   end
@@ -14,7 +12,6 @@ class Cms::AddressesController < Cms::SecuredController
   end
 
   def edit
-    @address = @real_estate.address
   end
 
   def create
@@ -29,8 +26,6 @@ class Cms::AddressesController < Cms::SecuredController
   end
 
   def update
-    @address = @real_estate.address
-
     if @address.update_attributes(params[:address])
       redirect_to_step('information')
     else
