aeea6e29096a409c476de393343bf22eee351aaa
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-09 13:25:30 +0100

    cleanup because of object loading using CanCan helper

--- app/controllers/cms/pricings_controller.rb
+++ app/controllers/cms/pricings_controller.rb
@@ -1,11 +1,10 @@
 class Cms::PricingsController < Cms::SecuredController
   include EmbeddedInRealEstate
 
-  authorize_resource :only => [:edit, :update], :through=>:real_estate
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to cms_real_estate_pricing_path(@real_estate), :alert => exception.message
   end
+
 
   def new
     @pricing = Pricing.new
@@ -13,13 +12,9 @@ class Cms::PricingsController < Cms::SecuredController
   end
 
   def edit
-    @pricing = @real_estate.pricing
   end
 
   def create
-    @pricing = Pricing.new(params[:pricing])
-    @pricing.real_estate = @real_estate
-
     if @pricing.save
       redirect_to_step('figure')
     else
@@ -28,8 +23,6 @@ class Cms::PricingsController < Cms::SecuredController
   end
 
   def update
-    @pricing = @real_estate.pricing
-
     if @pricing.update_attributes(params[:pricing])
       redirect_to_step('figure')
     else
@@ -38,7 +31,6 @@ class Cms::PricingsController < Cms::SecuredController
   end
   
   def show
-    @pricing = @real_estate.pricing
   end
   
 end
