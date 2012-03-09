65f6b51c3c1516e027e405b9314eacc2f84629bf
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-09 13:21:40 +0100

    refactor object loading using CanCan helper, add authorization specs, fix broken specs

--- spec/controllers/cms/information_controller_spec.rb
+++ spec/controllers/cms/information_controller_spec.rb
@@ -5,12 +5,13 @@ describe 'Real Estate Wizard' do
   login_cms_user
   disable_sweep!
 
-  let :real_estate do
-    mock_model(RealEstate, :save => true, :update_attributes => true)
-  end
 
   describe Cms::InformationController do
     describe '#create' do
+      let :real_estate do
+        mock_model(RealEstate, :save => true, :update_attributes => true)
+      end
+
       it 'redirects to the new pricing tab without an existing pricing' do
         mock = real_estate
         mock.stub!(:pricing).and_return(nil)
@@ -36,30 +37,48 @@ describe 'Real Estate Wizard' do
       end
     end
 
+
     describe '#update' do
-      it 'redirects to the new information tab without an existing information' do
-        mock = real_estate
-        mock.stub!(:pricing).and_return(nil)
-        mock.stub!(:information).and_return(mock_model(Information, :update_attributes => true))
+      before do
+        @real_estate = Fabricate :real_estate, :address => Fabricate.build(:address), :category => Fabricate(:category), :information => Fabricate.build(:information)
+      end
 
-        RealEstate.stub!(:find).and_return(mock)
-
-        post :update, :real_estate_id => mock.id
-        response.should redirect_to(new_cms_real_estate_pricing_path(mock))
+      it 'redirects to the new pricing tab without an existing pricing' do
+        put :update, :real_estate_id => @real_estate.id, :information=>Fabricate.attributes_for(:information)
+        response.should redirect_to(new_cms_real_estate_pricing_path(@real_estate))
         flash[:success].should_not be_nil
       end
 
-      it 'redirects to the new information tab with an existing information' do
-        mock = real_estate
-        mock.stub!(:pricing).and_return(mock_model(Pricing))
-        mock.stub!(:information).and_return(mock_model(Information, :update_attributes => true))
+      it 'redirects to the new pricing tab with an existing pricing' do
+        @real_estate.pricing = Fabricate.build(:pricing)
 
-        RealEstate.stub!(:find).and_return(mock)
-
-        post :update, :real_estate_id => mock.id
-        response.should redirect_to(edit_cms_real_estate_pricing_path(mock))
+        put :update, :real_estate_id => @real_estate.id
+        response.should redirect_to(edit_cms_real_estate_pricing_path(@real_estate))
         flash[:success].should_not be_nil
       end
     end
+
+
+    describe '#authentication' do
+      context "Real estate isn't editable" do
+        before do
+          @real_estate = Fabricate :published_real_estate, :category => Fabricate(:category), :address => Fabricate.build(:address), :information => Fabricate.build(:information)
+          @access_denied = "Sie haben keine Berechtigungen für diese Aktion"
+        end
+
+        it 'prevents from accessing #edit' do
+          get :edit, :real_estate_id => @real_estate.id
+          response.should redirect_to [:cms, @real_estate, :information]
+          flash[:alert].should == @access_denied
+        end
+
+        it 'prevents from accessing #update' do
+          post :update, :real_estate_id => @real_estate.id
+          response.should redirect_to [:cms, @real_estate, :information]
+          flash[:alert].should == @access_denied
+        end
+      end
+    end
+
   end
 end
