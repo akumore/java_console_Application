d4f146ac38271862c8154ee4d9cb6cdb82dbb5a3
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-11 11:19:05 +0100

    fix broken spec

--- spec/controllers/cms/additional_descriptions_controller_spec.rb
+++ spec/controllers/cms/additional_descriptions_controller_spec.rb
@@ -6,32 +6,22 @@ describe 'Real Estate Wizard' do
   disable_sweep!
 
   let :real_estate do
-    mock_model(RealEstate, :save => true, :update_attributes => true)
+    Fabricate :real_estate, :category=>Fabricate(:category)
   end
 
   describe Cms::AdditionalDescriptionsController do
     describe '#create' do
       it 'redirects to the media assets overview tab' do
-        mock = real_estate
-
-        RealEstate.stub!(:find).and_return(mock)
-        AdditionalDescription.stub!(:new).and_return(mock_model(AdditionalDescription, :save => true, :real_estate= => nil))
-
-        post :create, :real_estate_id => mock.id
-        response.should redirect_to(cms_real_estate_media_assets_path(mock))
+        post :create, :real_estate_id => real_estate.id, :additional_description=>Fabricate.attributes_for(:additional_description)
+        response.should redirect_to(cms_real_estate_media_assets_path(real_estate))
         flash[:success].should_not be_nil
       end
     end
 
     describe '#create' do
       it 'redirects to the media assets overview tab' do
-        mock = real_estate
-        mock.stub!(:additional_descriptions).and_return(mock_model(AdditionalDescription, :update_attributes => true))
-
-        RealEstate.stub!(:find).and_return(mock)
-
-        post :update, :real_estate_id => mock.id
-        response.should redirect_to(cms_real_estate_media_assets_path(mock))
+        put :update, :real_estate_id => real_estate.id, :additional_description=>Fabricate.attributes_for(:additional_description)
+        response.should redirect_to(cms_real_estate_media_assets_path(real_estate))
         flash[:success].should_not be_nil
       end
     end
