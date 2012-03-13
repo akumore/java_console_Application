769e23b058bab22aa03729d5f8845cc78f255d79
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-13 16:33:27 +0100

    fix spec

--- spec/controllers/cms/additional_descriptions_controller_spec.rb
+++ spec/controllers/cms/additional_descriptions_controller_spec.rb
@@ -5,23 +5,27 @@ describe 'Real Estate Wizard' do
   login_cms_user
   disable_sweep!
 
-  let :real_estate do
-    Fabricate :real_estate, :category=>Fabricate(:category)
-  end
-
   describe Cms::AdditionalDescriptionsController do
     describe '#create' do
+      before do
+        @real_estate = Fabricate :real_estate, :category => Fabricate(:category)
+      end
+
       it 'redirects to the media assets overview tab' do
-        post :create, :real_estate_id => real_estate.id, :additional_description=>Fabricate.attributes_for(:additional_description)
-        response.should redirect_to(cms_real_estate_media_assets_path(real_estate))
+        post :create, :real_estate_id => @real_estate.id, :additional_description => Fabricate.attributes_for(:additional_description)
+        response.should redirect_to(cms_real_estate_media_assets_path(@real_estate))
         flash[:success].should_not be_nil
       end
     end
 
-    describe '#create' do
+    describe '#update' do
+      before do
+        @real_estate = Fabricate :real_estate, :category => Fabricate(:category), :additional_description => Fabricate.build(:additional_description)
+      end
+
       it 'redirects to the media assets overview tab' do
-        put :update, :real_estate_id => real_estate.id, :additional_description=>Fabricate.attributes_for(:additional_description)
-        response.should redirect_to(cms_real_estate_media_assets_path(real_estate))
+        put :update, :real_estate_id => @real_estate.id, :additional_description => Fabricate.attributes_for(:additional_description)
+        response.should redirect_to(cms_real_estate_media_assets_path(@real_estate))
         flash[:success].should_not be_nil
       end
     end
