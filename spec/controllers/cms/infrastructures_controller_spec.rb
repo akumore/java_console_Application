e79ff1628cbab96d5f8bbcf747c30f16f9a28daa
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-15 09:41:45 +0100

    fix broken spec

--- spec/controllers/cms/infrastructures_controller_spec.rb
+++ spec/controllers/cms/infrastructures_controller_spec.rb
@@ -5,36 +5,32 @@ describe 'Real Estate Wizard' do
   login_cms_user
   disable_sweep!
 
-  let :real_estate do
-    mock_model(RealEstate, :save => true, :update_attributes => true)
-  end
-
   describe Cms::InfrastructuresController do
+
+    let :infrastructure_attributes do
+      @infrastructure_attributes = Fabricate.attributes_for(:infrastructure)
+    end
+
     describe '#create' do
+      before do
+        @real_estate = Fabricate :real_estate, :category => Fabricate(:category)
+      end
+
       it 'redirects to the new descriptions tab without an existing description' do
-        mock = real_estate
-        mock.stub!(:additional_description).and_return(nil)
-
-        RealEstate.stub!(:find).and_return(mock)
-        Infrastructure.stub!(:new).and_return(mock_model(Infrastructure, :save => true, :real_estate= => nil))
-
-        post :create, :real_estate_id => mock.id
-        response.should redirect_to(new_cms_real_estate_additional_description_path(mock))
+        post :create, :real_estate_id => @real_estate.id, :infrastructure => infrastructure_attributes
+        response.should redirect_to new_cms_real_estate_additional_description_path(@real_estate)
         flash[:success].should_not be_nil
       end
 
       it 'redirects to the edit descriptions tab with an existing description' do
-        mock = real_estate
-        mock.stub!(:additional_description).and_return(mock_model(AdditionalDescription))
-        
-        RealEstate.stub!(:find).and_return(mock)        
-        Infrastructure.stub!(:new).and_return(mock_model(Infrastructure, :save => true, :real_estate= => nil))
+        @real_estate.additional_description = Fabricate.build :additional_description
 
-        post :create, :real_estate_id => mock.id
-        response.should redirect_to(edit_cms_real_estate_additional_description_path(mock))
+        post :create, :real_estate_id => @real_estate.id, :infrastructure => infrastructure_attributes
+        response.should redirect_to edit_cms_real_estate_additional_description_path(@real_estate)
         flash[:success].should_not be_nil
       end
     end
+
 
     describe '#update' do
       before do
@@ -42,14 +38,15 @@ describe 'Real Estate Wizard' do
       end
 
       it 'redirects to the new descriptions tab without an existing description' do
-        put :update, :real_estate_id => @real_estate.id, :infrastructure=>Fabricate.attributes_for(:infrastructure)
+        put :update, :real_estate_id => @real_estate.id, :infrastructure => infrastructure_attributes
         response.should redirect_to(new_cms_real_estate_additional_description_path(@real_estate))
         flash[:success].should_not be_nil
       end
 
       it 'redirects to the descriptions tab with an existing description' do
         @real_estate.additional_description = Fabricate.build(:additional_description)
-        put :update, :real_estate_id => @real_estate.id, :infrastructure=>Fabricate.attributes_for(:infrastructure)
+
+        put :update, :real_estate_id => @real_estate.id, :infrastructure => infrastructure_attributes
         response.should redirect_to(edit_cms_real_estate_additional_description_path(@real_estate))
         flash[:success].should_not be_nil
       end
@@ -70,7 +67,7 @@ describe 'Real Estate Wizard' do
         end
 
         it 'prevents from accessing #update' do
-          put :update, :real_estate_id => @real_estate.id, :infrastructure=>Fabricate.attributes_for(:infrastructure)
+          put :update, :real_estate_id => @real_estate.id, :infrastructure => infrastructure_attributes
           response.should redirect_to [:cms, @real_estate, :infrastructure]
           flash[:alert].should == @access_denied
         end
