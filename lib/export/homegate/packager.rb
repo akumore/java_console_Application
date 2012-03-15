6e2e4aa00f5f831563d0ddf9f7eb84e506177ab3
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-03-15 20:04:29 +0100

    enhance packager spec suite

--- lib/export/homegate/packager.rb
+++ lib/export/homegate/packager.rb
@@ -1,13 +1,18 @@
 module Export
   module Homegate
     class Packager
+      def initialize
+        @time = Time.now
+        @date_folder = @time.strftime("%Y_%m_%d")
+        @time_folder = @time.strftime("%H_%M")
+      end
+
       def package(real_estate)
         Homegate::RealEstatePackage.new(real_estate).save
       end
 
       def path
-        time = Time.now
-        File.join root_path, time.strftime("%Y_%m_%d"), time.strftime("%H_%M")
+        File.join root_path, @date_folder, @time_folder
       end
 
       def root_path
--- spec/lib/export/homegate/packager_spec.rb
+++ spec/lib/export/homegate/packager_spec.rb
@@ -10,16 +10,17 @@ describe Export::Homegate::Packager do
 
   describe '#path' do
     it 'returns the path for storing an export' do
-      packager = Export::Homegate::Packager.new
-
       time_travel_to(Time.new(2012, 5, 20, 16, 35)) do
+        packager = Export::Homegate::Packager.new
         packager.path.should == "#{packager.root_path}/2012_05_20/16_35"
       end
     end
   end
 
   describe '#root_path' do
-    packager = Export::Homegate::Packager.new
-    packager.root_path.should == "#{Rails.root}/tmp/export"
+    it 'returns the root path for all export folders' do
+      packager = Export::Homegate::Packager.new
+      packager.root_path.should == "#{Rails.root}/tmp/export"
+    end
   end
 end
