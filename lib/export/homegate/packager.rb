fc3e859064571718db255c636797f6880587611c
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-16 16:32:17 +0100

    create folder structure

--- lib/export/homegate/packager.rb
+++ lib/export/homegate/packager.rb
@@ -1,10 +1,12 @@
 module Export
   module Homegate
     class Packager
+
       def initialize
         @time = Time.now
         @date_folder = @time.strftime("%Y_%m_%d")
         @time_folder = @time.strftime("%H_%M")
+        create_folders
       end
 
       def package(real_estate)
@@ -18,6 +20,16 @@ module Export
       def root_path
         File.join Rails.root, 'tmp', 'export'
       end
+
+      private
+      def create_folders
+        FileUtils.mkdir_p path
+        FileUtils.mkdir_p File.join(path, 'data')
+        FileUtils.mkdir_p File.join(path, 'images')
+        FileUtils.mkdir_p File.join(path, 'movies')
+        FileUtils.mkdir_p File.join(path, 'docs')
+      end
+
     end
   end
 end
