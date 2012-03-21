e6f019b3f56cfcd2a1fdb1eefb6ffae96ff01e6a
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-03-21 16:42:22 +0100

    fix: add new production url

--- config/blazing.rb
+++ config/blazing.rb
@@ -4,7 +4,7 @@
 #
 repository 'git@github.com:screenconcept/alflred_mueller.git'
 
-target :production, 'alfred_mueller@scrcpt2.nine.ch:/home/usr/alfred_mueller/public_html', :url => 'http://alfredmueller.screenconcept.ch', :rails_env => 'production'
+target :production, 'alfred_mueller@scrcpt2.nine.ch:/home/usr/alfred_mueller/public_html', :url => 'http://production.alfredmueller.screenconcept.ch', :rails_env => 'production'
 target :staging, 'amstaging@scrcpt2.nine.ch:/home/usr/amstaging/public_html', :url => 'http://staging.alfredmueller.screenconcept.ch', :rails_env => 'staging'
 
 rvm :rvmrc
