0b6ca3b3ad9d5c204cad354f47d9ea3df8258d33
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-03-27 15:37:12 +0200

    add contact model

---
+++ spec/fabricators/contact_fabricator.rb
@@ -0,0 +1,8 @@
+Fabricator(:contact) do
+  name 'Hans Muster'
+  street 'Musterstrasse 88'
+  zip '9323'
+  city 'Musterstadt'
+  email 'hans.muster@domain.ch'
+  message 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'  
+end
