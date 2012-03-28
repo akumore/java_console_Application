e51ea686ec3b0a2a312a4e74d261ea79b8ef8692
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-28 17:24:01 +0200

    add support of multi-parameter attributes

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,6 +1,7 @@
 class NewsItem
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::MultiParameterAttributes
 
   field :title, :type => String
   field :teaser, :type => String
