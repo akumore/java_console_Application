ad8e683ff572d29c315c2a463313b15ca2217ec3
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-28 14:29:51 +0200

    fix news item fabricator

--- spec/fabricators/news_item_fabricator.rb
+++ spec/fabricators/news_item_fabricator.rb
@@ -1,5 +1,5 @@
-Fabricator(:page) do
-  title sequence(:title) { |n| "News Title #{n}" }
+Fabricator(:news_item) do
+  title { sequence(:title) { |n| "News Title #{n}" } }
   teaser "News Teaser shown in Page Footer"
   content "News Content News Content News Content"
   date Date.today
