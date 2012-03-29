e41d379569f136534550a3bc4b48a6770601f62d
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-03-29 10:11:53 +0200

    add action mailer default url options for staging and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,4 +58,6 @@ AlfredMueller::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'production.alfredmueller.screenconcept.ch' }
 end
