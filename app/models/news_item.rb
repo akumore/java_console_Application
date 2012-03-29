7a058687c4cdd1cdf154c3bdc6f131a4181c1e12
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-29 18:16:17 +0200

    add NewsItem, NewsItemImage and NewsItemDocument

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -3,6 +3,12 @@ class NewsItem
   include Mongoid::Timestamps
   include Mongoid::MultiParameterAttributes
 
+  accepts_nested_attributes_for :images, :allow_destroy => true
+  embeds_many :images, :class_name=>"NewsItemImage", cascade_callbacks: true
+
+  accepts_nested_attributes_for :documents, :allow_destroy => true
+  embeds_many :documents, :class_name=>"NewsItemDocument", cascade_callbacks: true
+
   field :title, :type => String
   field :teaser, :type => String
   field :content, :type => String
