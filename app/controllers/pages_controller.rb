e04fc4ca3117f5e0a6c55d166fc3777e0844d791
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-30 13:05:08 +0200

    refactor page validation of name uniqueness, scope it by locale

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   respond_to :html
 
   def show
-    @page = Page.where(:name => params[:name]).first
+    @page = Page.where(:name => params[:name], :locale=>I18n.locale).first
     respond_with @page
   end
 end
