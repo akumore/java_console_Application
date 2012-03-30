4353ffe26d506200cf2dd0d9415c5d12d5936b70
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-30 14:19:17 +0200

    raise and rescue on (CMS) page not found

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,20 @@ class PagesController < ApplicationController
 
   respond_to :html
 
+  rescue_from Mongoid::Errors::DocumentNotFound do |err|
+    logger.warn [err.class, err].join(": ")
+    redirect_to root_path
+  end
+
   def show
-    @page = Page.where(:name => params[:name], :locale=>I18n.locale).first
+    @page = Page.where(query_params).first or raise Mongoid::Errors::DocumentNotFound.new(Page, query_params)
     respond_with @page
   end
+
+
+  private
+  def query_params
+    {:name => params[:name], :locale=>I18n.locale}
+  end
+
 end
