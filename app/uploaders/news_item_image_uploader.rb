5b79da26544b576051fa7f1deb5f083cc6a7755d
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-03-30 11:18:35 +0200

    image uploader create version used for gallery

--- app/uploaders/news_item_image_uploader.rb
+++ app/uploaders/news_item_image_uploader.rb
@@ -6,6 +6,10 @@ class NewsItemImageUploader < BaseUploader
     process :resize_to_fill => [100,100]
   end
 
+  version :gallery do
+    process :resize_to_fill => [1000,500]
+  end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
