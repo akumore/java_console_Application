320e85b312e495963fc1051ccc7e8e17872a7f7d
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-04-02 13:02:11 +0200

    add locale to job model

--- spec/fabricators/job_fabricator.rb
+++ spec/fabricators/job_fabricator.rb
@@ -3,6 +3,7 @@ Fabricator(:job) do
   text '## Text als Markdown'
   is_published false
   job_profile_file File.open("#{Rails.root}/spec/support/test_files/document.pdf")
+  locale 'de'
 end
 
 Fabricator(:published_job, :from => :job) do
