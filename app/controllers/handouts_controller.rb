d7303aab76d94914d36c3e7141ee18bd9044da96
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-04-04 17:28:33 +0200

    add basic mini doku setup configuration

--- app/controllers/handouts_controller.rb
+++ app/controllers/handouts_controller.rb
@@ -2,6 +2,6 @@ class HandoutsController < ApplicationController
   layout 'handout'
   
   def show
-    
+    @real_estate = RealEstate.find(params[:id])
   end
 end
---
+++ config/initializers/pdf_kit.rb
@@ -0,0 +1,9 @@
+PDFKit.configure do |config|
+  config.default_options = {
+    :page_size     => 'Letter',
+    :margin_top    => '0in',
+    :margin_right  => '0in',
+    :margin_bottom => '0in',
+    :margin_left   => '0in'
+  }
+end
