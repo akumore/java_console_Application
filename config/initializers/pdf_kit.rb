f2fd045c194584fc1bef177bb6a091340ab8f7ab
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-04-05 11:35:50 +0200

    add basic content styles for mini doku

--- config/initializers/pdf_kit.rb
+++ config/initializers/pdf_kit.rb
@@ -1,9 +1,5 @@
 PDFKit.configure do |config|
   config.default_options = {
-    :page_size     => 'Letter',
-    :margin_top    => '0in',
-    :margin_right  => '0in',
-    :margin_bottom => '0in',
-    :margin_left   => '0in'
+    :page_size     => 'A4'
   }
 end
