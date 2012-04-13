8eff23e3f641c2b7ec017e0ce0105d2e3f41ae19
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-04-13 15:56:39 +0200

    use a static version of wkhtmltopdf

--- config/initializers/pdf_kit.rb
+++ config/initializers/pdf_kit.rb
@@ -1,4 +1,10 @@
 PDFKit.configure do |config|
+  if RUBY_PLATFORM =~ /darwin/
+    config.wkhtmltopdf = Rails.root.join('vendor/wkhtmltopdf/bin/wkhtmltopdf.app/Contents/MacOS/wkhtmltopdf').to_s
+  else
+    config.wkhtmltopdf = Rails.root.join('vendor/wkhtmltopdf/bin/wkhtmltopdf-amd64').to_s
+  end
+
   config.default_options = {
     :page_size     => 'A4'
   }
