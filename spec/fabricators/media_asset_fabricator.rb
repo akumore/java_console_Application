897cb9d18de523bf36500eb037589321ff4519f6
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-04-18 15:03:06 +0200

    don't set position, it's done by the model

--- spec/fabricators/media_asset_fabricator.rb
+++ spec/fabricators/media_asset_fabricator.rb
@@ -3,7 +3,6 @@ Fabricator(:media_asset_image, :class_name => 'MediaAsset') do
   is_primary false
   file File.open("#{Rails.root}/spec/support/test_files/image.jpg")
   title "Image title"
-  position { Fabricate.sequence }
 end
 
 Fabricator(:media_asset_image_png, :class_name => 'MediaAsset') do
@@ -11,7 +10,6 @@ Fabricator(:media_asset_image_png, :class_name => 'MediaAsset') do
   is_primary false
   file File.open("#{Rails.root}/spec/support/test_files/image.png")
   title "Image title"
-  position { Fabricate.sequence }
 end
 
 Fabricator(:media_asset_floorplan, :from => :media_asset_image) do
@@ -23,7 +21,6 @@ Fabricator(:media_asset_video, :class_name => 'MediaAsset') do
   is_primary false
   file File.open("#{Rails.root}/spec/support/test_files/video.mp4")
   title "Video title"
-  position { Fabricate.sequence }
 end
 
 Fabricator(:media_asset_document, :class_name => 'MediaAsset') do
@@ -31,5 +28,4 @@ Fabricator(:media_asset_document, :class_name => 'MediaAsset') do
   is_primary false
   file File.open("#{Rails.root}/spec/support/test_files/document.pdf")
   title "Document title"
-  position { Fabricate.sequence }
 end
