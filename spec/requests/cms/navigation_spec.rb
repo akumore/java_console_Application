82f2864b7c85529c58ab1238b880b5cf1c2fa94a
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-04-19 12:29:20 +0200

    hide navigation links the user isn't authorized for

---
+++ spec/requests/cms/navigation_spec.rb
@@ -0,0 +1,107 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe "Cms Navigation" do
+
+  shared_examples_for "navigation menu for all user roles" do
+    it "links to the dashboard" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Dashboard", :href => cms_dashboards_path
+      end
+    end
+
+    it "links to the real estate wizard" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Immobilien", :href => cms_real_estates_path
+      end
+    end
+
+    it "provides the logout link" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Logout", :href => destroy_user_session_path
+      end
+    end
+
+    it "links to the user administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Benutzer", :href => cms_users_path
+      end
+    end
+  end
+
+
+  context "As an admin" do
+    login_cms_admin
+
+    it_behaves_like "navigation menu for all user roles"
+
+    it "links to the employees/contact persons administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Mitarbeiter", :href => cms_employees_path
+      end
+    end
+
+
+    it "links to the news administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "News", :href => cms_news_items_path
+      end
+    end
+
+    it "links to the jobs administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Jobs", :href => cms_jobs_path
+      end
+    end
+
+    it "links to the pages administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should have_link "Seiten", :href => cms_pages_path
+      end
+    end
+  end
+
+
+  context "As an editor" do
+    login_cms_editor
+
+    it_behaves_like "navigation menu for all user roles"
+
+    it "doesn't link to the employees/contact persons administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should_not have_link "Mitarbeiter", :href => cms_employees_path
+      end
+    end
+
+    it "doesn't link to the news administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should_not have_link "News", :href => cms_news_items_path
+      end
+    end
+
+    it "doesn't link to the jobs administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should_not have_link "Jobs", :href => cms_jobs_path
+      end
+    end
+
+    it "doesn't link to the pages administration" do
+      visit cms_dashboards_path
+      within ".navbar" do
+        page.should_not have_link "Seiten", :href => cms_pages_path
+      end
+    end
+  end
+
+end
