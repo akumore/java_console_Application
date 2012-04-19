5be76e9337344c2584108bbaa55e68a8438cc9ed
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-04-19 11:42:33 +0200

    refactor authorization of cms users controller

--- app/controllers/cms/users_controller.rb
+++ app/controllers/cms/users_controller.rb
@@ -1,5 +1,6 @@
 class Cms::UsersController < Cms::SecuredController
-  
+
+  authorize_resource :except=>:index
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to cms_dashboards_path, :alert => exception.message
   end
@@ -11,13 +12,11 @@ class Cms::UsersController < Cms::SecuredController
 
   def new
     @user = Cms::User.new
-    authorize! :new, @user
     respond_with @user
   end
 
   def create
     @user = Cms::User.new(params[:cms_user])
-    authorize! :create, @user
     if @user.save
       flash[:success] = t("cms.users.create.success", :email=>@user.email)
     end
@@ -26,13 +25,11 @@ class Cms::UsersController < Cms::SecuredController
 
   def edit
     @user = Cms::User.find(params[:id])
-    authorize! :edit, @user
     respond_with @user
   end
 
   def update
     @user = Cms::User.find(params[:id])
-    authorize! :update, @user
 
     # update differently depending if a new password was provided
     any_passwords = %w(password password_confirmation).any? do |field|
@@ -52,7 +49,6 @@ class Cms::UsersController < Cms::SecuredController
 
   def destroy
     @user = Cms::User.find(params[:id])
-    authorize! :destroy, @user
     if @user.destroy
       flash[:success] = t("cms.users.destroy.success", :email=>@user.email)
     end
