ab9f7869854f5bc11874ad539a605d9f8cdd2cd8
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-04-26 15:58:19 +0200

    refactor media assets controller

--- app/controllers/cms/media_assets_controller.rb
+++ app/controllers/cms/media_assets_controller.rb
@@ -1,48 +1,10 @@
 class Cms::MediaAssetsController < Cms::SecuredController
   include Concerns::EmbeddedInRealEstate
 
-  load_resource :through => :real_estate
-  authorize_resource :through => :real_estate, :except => [:show, :index]
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to cms_real_estate_media_assets_path(@real_estate), :alert => exception.message
-  end
-  
-  
   def index
-    @media_assets = @real_estate.media_assets
-    respond_with @media_assets
-  end
-
-  def show
-  end
-
-  def new
-    @media_asset = MediaAsset.new(:media_type => params[:media_type])
-    respond_with @media_asset  
-  end
-
-  def edit
-  end
-
-  def create
-    if @media_asset.save
-      redirect_to edit_cms_real_estate_media_asset_path(@real_estate, @media_asset)
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    if @media_asset.update_attributes(params[:media_asset])
-      redirect_to edit_cms_real_estate_media_asset_path(@real_estate, @media_asset)
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @media_asset.destroy
-    redirect_to cms_real_estate_media_assets_url
+    #images are accessed using the form builder
+    @floor_plans = @real_estate.floor_plans
+    @videos = @real_estate.videos
+    @documents = @real_estate.documents
   end
 end
