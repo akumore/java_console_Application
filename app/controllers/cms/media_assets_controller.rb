d9e05ff465b0248b5f72a98a7f08ae95711d00b0
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-04-27 12:16:39 +0200

    make all kind of assets sortable

--- app/controllers/cms/media_assets_controller.rb
+++ app/controllers/cms/media_assets_controller.rb
@@ -2,9 +2,5 @@ class Cms::MediaAssetsController < Cms::SecuredController
   include Concerns::EmbeddedInRealEstate
 
   def index
-    #images are accessed using the form builder
-    @floor_plans = @real_estate.floor_plans
-    @videos = @real_estate.videos
-    @documents = @real_estate.documents
   end
 end
