0147bafa336e2b0f2deacfe11ec721a9606c88a1
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-05-03 17:53:45 +0200

    fix: remove unused poi with nil distances

--- db/migrate/20120503144430_remove_duplicate_poi.rb
+++ db/migrate/20120503144430_remove_duplicate_poi.rb
@@ -2,7 +2,10 @@ class RemoveDuplicatePoi < Mongoid::Migration
   def self.up
     RealEstate.all.each do |real_estate|
       if real_estate.infrastructure.present?
-        real_estate.infrastructure.points_of_interest.where(:distance => '', :distance => nil).map(&:destroy)
+        real_estate.infrastructure.points_of_interest.where(:distance => '').map(&:destroy)
+        real_estate.infrastructure.points_of_interest.where(:distance => nil).map(&:destroy)
+
+
         PointOfInterest::TYPES.each do |type|
           if real_estate.infrastructure.points_of_interest.where(:name => type).length > 1
             real_estate.infrastructure.points_of_interest.where(:name => type).skip(1).map(&:destroy)
