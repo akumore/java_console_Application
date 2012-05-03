aa736bf59774422ef97068a2e282d422430397bc
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-05-03 17:45:58 +0200

    clear empty poi, use destroy

--- db/migrate/20120503144430_remove_duplicate_poi.rb
+++ db/migrate/20120503144430_remove_duplicate_poi.rb
@@ -2,11 +2,13 @@ class RemoveDuplicatePoi < Mongoid::Migration
   def self.up
     RealEstate.all.each do |real_estate|
       if real_estate.infrastructure.present?
+        real_estate.infrastructure.points_of_interest.where(:distance => '', :distance => nil).map(&:destroy)
         PointOfInterest::TYPES.each do |type|
           if real_estate.infrastructure.points_of_interest.where(:name => type).length > 1
-            real_estate.infrastructure.points_of_interest.where(:name => type).skip(1).map(&:delete)
+            real_estate.infrastructure.points_of_interest.where(:name => type).skip(1).map(&:destroy)
           end
         end
+
       end
     end
   end
