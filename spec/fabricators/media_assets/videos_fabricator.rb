08317c83a357d40cd077ee2f76a0d8d9b3fbc7bb
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-05-04 12:51:04 +0200

    fix copy&paste

--- spec/fabricators/media_assets/videos_fabricator.rb
+++ spec/fabricators/media_assets/videos_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator :media_assets_video, :class_name => MediaAssets::Video do
-  title "Image title"
+  title "Video title"
   file File.open("#{Rails.root}/spec/support/test_files/video.mp4")
 end
