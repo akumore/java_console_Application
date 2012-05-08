aff2704b51435b8a9014d10c190a3be371491278
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-05-08 14:33:33 +0200

    update spec while searching for strange bug when running all request specs

--- spec/requests/navigation_spec.rb
+++ spec/requests/navigation_spec.rb
@@ -2,19 +2,20 @@
 require 'spec_helper'
 
 describe 'Language Navigation' do
+  monkey_patch_default_url_options
 
-  %w(de en fr it).each do |locale|
-    it "links to the language version #{locale.upcase}" do
-      visit root_path :locale => locale
+  %w(de en fr it).each do |lang|
+    it "links to the language version #{lang.upcase}" do
+      visit root_path(:locale => lang)
       within '.meta-navigation' do
-        page.should have_link locale, :href => root_path(:locale => locale)
+        page.should have_link lang, :href => root_path(:locale => lang)
       end
     end
 
-    it "links to contacts for #{locale.upcase}" do
-      visit root_path :locale => locale
+    it "links to contacts for #{lang.upcase}" do
+      visit root_path(:locale => lang)
       within '.meta-navigation' do
-        page.should have_link I18n.t('navigation.meta.contact'), :href => contact_path(:locale => locale)
+        page.should have_link I18n.t('navigation.meta.contact'), :href => contact_path(:locale => lang)
       end
     end
   end
@@ -23,43 +24,44 @@ end
 
 
 describe "Main Navigation" do
+  monkey_patch_default_url_options
 
-  shared_examples_for "All language versions" do |locale|
+  shared_examples_for "All language versions" do |lang|
     it 'links to the list of real estates' do
-      visit root_path :locale => locale
+      visit root_path(:locale => lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.real_estate'), :href => real_estates_path(:locale => locale)
+        page.should have_link I18n.t('navigation.main.real_estate'), :href => real_estates_path(:locale => lang)
       end
     end
 
     it "links to the content page 'jobs'" do
-      visit root_path :locale => locale
+      visit root_path(:locale => lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.jobs'), :href => "/#{locale}/jobs"
+        page.should have_link I18n.t('navigation.main.jobs'), :href => "/#{lang}/jobs"
       end
     end
 
     it "links to the content page 'company'" do
-      visit root_path :locale => locale
+      visit root_path(:locale => lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.company'), :href => "/#{locale}/company"
+        page.should have_link I18n.t('navigation.main.company'), :href => "/#{lang}/company"
       end
     end
 
     it "links to the news" do
-      visit root_path :locale => locale
+      visit root_path(:locale => lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.news'), :href => news_items_path(:locale => locale)
+        page.should have_link I18n.t('navigation.main.news'), :href => news_items_path(:locale => lang)
       end
     end
   end
 
 
-  shared_examples_for "All languages but 'DE'" do |locale|
+  shared_examples_for "All languages but 'DE'" do |lang|
     it "has no link to the content page 'knowledge'" do
-      visit root_path :locale => locale
+      visit root_path(:locale => lang)
       within '.main-navigation' do
-        page.should_not have_link I18n.t('navigation.main.knowledge'), :href => "/#{locale}/knowledge"
+        page.should_not have_link I18n.t('navigation.main.knowledge'), :href => "/#{lang}/knowledge"
       end
     end
   end
@@ -77,10 +79,10 @@ describe "Main Navigation" do
   end
 
 
-  %w(fr en it).each do |locale|
-    context "Language '#{locale.upcase}'" do
-      it_behaves_like "All language versions", locale
-      it_behaves_like "All languages but 'DE'", locale
+  %w(fr en it).each do |lang|
+    context "Language '#{lang.upcase}'" do
+      it_behaves_like "All language versions", lang
+      it_behaves_like "All languages but 'DE'", lang
     end
   end
 
