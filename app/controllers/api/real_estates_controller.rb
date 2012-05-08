ab3544dd4b53fa22af7199be30221a0e1d462499
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-05-08 09:37:06 +0200

    micro-site api limits collection to micro-site enabled real estates

--- app/controllers/api/real_estates_controller.rb
+++ app/controllers/api/real_estates_controller.rb
@@ -4,7 +4,7 @@ module Api
     respond_to :json
 
     def index
-      @real_estates = RealEstate.limit(1).all #microsite.published
+      @real_estates = RealEstate.microsite.published.all
       respond_with @real_estates, :callback=>params[:callback]
     end
 
