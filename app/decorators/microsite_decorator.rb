1431b507f0f87b0006429c9ca6f37822b06bbb06
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-05-10 10:30:15 +0200

    add microsite decorator

---
+++ app/decorators/microsite_decorator.rb
@@ -0,0 +1,29 @@
+class MicrositeDecorator < ApplicationDecorator
+  include Draper::LazyHelpers
+
+  decorates :real_estate
+
+  def id
+    
+  end
+
+  def rooms
+    
+  end
+
+  def floor_label
+    
+  end
+
+  def house
+    
+  end
+
+  def surface
+    
+  end
+
+  def price
+    
+  end
+end
