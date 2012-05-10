89aa535bfd2803e8488b1862bd0ce130f4a08786
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-05-10 10:55:21 +0200

    add microsite decorator

--- app/controllers/api/real_estates_controller.rb
+++ app/controllers/api/real_estates_controller.rb
@@ -4,7 +4,7 @@ module Api
     respond_to :json
 
     def index
-      @real_estates = RealEstate.microsite.published.all
+      @real_estates = MicrositeDecorator.decorate RealEstate.microsite.published.all
       respond_with @real_estates, :callback=>params[:callback]
     end
 
