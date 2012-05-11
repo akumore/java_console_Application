37f72ddbfe4d4eebb2fa4b242b758c02df459603
Sebastian de Castelberg <sebu@kpricorn.org>
2012-05-11 17:13:37 +0200

    add microsite_decorator specs and implementation

--- app/decorators/microsite_decorator.rb
+++ app/decorators/microsite_decorator.rb
@@ -1,22 +1,47 @@
 class MicrositeDecorator < ApplicationDecorator
+
   include Draper::LazyHelpers
+
+  GARTENSTADT_STREET = 'Badenerstrasse'
+  STREET_NUMBER_HOUSE_MAP = {
+    '26' => 'M',
+    '28' => 'L',
+    '30' => 'K',
+    '32' => 'I',
+    '34' => 'H',
+  }
+
+  FLOOR_FLOOR_LABEL_MAP = {
+    -1 => 'UG',
+    0  => 'EG',
+    1  => '1.OG',
+    2  => '2.OG',
+    3  => '3.OG',
+    4  => '4.OG',
+  }
 
   decorates :real_estate
 
-  def id
-    
-  end
-
   def rooms
-    
+    real_estate.figure.rooms
   end
 
   def floor_label
-    
+    figure = real_estate.figure
+    if figure.present?
+      return FLOOR_FLOOR_LABEL_MAP[figure.floor]
+    else
+      return nil
+    end
   end
 
   def house
-    
+    address = real_estate.address
+    if address.present? and address.street == GARTENSTADT_STREET then
+      return STREET_NUMBER_HOUSE_MAP[address.street_number]
+    else
+      return nil
+    end
   end
 
   def surface
@@ -24,6 +49,11 @@ class MicrositeDecorator < ApplicationDecorator
   end
 
   def price
-    
+    net_price = real_estate.pricing && real_estate.pricing.for_rent_netto
+    if net_price.nil?
+      return nil
+    else
+      return "CHF #{net_price}"
+    end
   end
 end
