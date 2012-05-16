6e81825dd2225d170537a02aca830f1273153bab
Sebastian de Castelberg <sebu@kpricorn.org>
2012-05-16 14:11:53 +0200

    add chapters pricing/infrastructure microsite decorator

--- app/decorators/infrastructure_decorator.rb
+++ app/decorators/infrastructure_decorator.rb
@@ -3,6 +3,50 @@ class InfrastructureDecorator < ApplicationDecorator
 
   decorates :infrastructure
 
+  def chapter
+    content = []
+    if infrastructure.inside_parking_spots.present?
+      content << {
+        :key => t('infrastructures.inside_parking_spots', :count => infrastructure.inside_parking_spots),
+        :value => infrastructure.inside_parking_spots
+      }
+    end
+
+    if infrastructure.outside_parking_spots.present?
+      content << {
+        :key => t('infrastructures.outside_parking_spots', :count => infrastructure.outside_parking_spots),
+        :value => infrastructure.outside_parking_spots
+      }
+    end
+
+    if infrastructure.inside_parking_spots_temporary.present?
+      content << {
+        :key => t('infrastructures.inside_parking_spots_temporary', :count => infrastructure.inside_parking_spots_temporary),
+        :value => infrastructure.inside_parking_spots_temporary
+      }
+    end
+
+    if infrastructure.outside_parking_spots_temporary.present?
+      content << {
+        :key => t('infrastructures.outside_parking_spots_temporary', :count => infrastructure.outside_parking_spots_temporary),
+        :value => infrastructure.outside_parking_spots_temporary
+      }
+    end
+
+    if distances.any?
+      content << {
+        :key => t('infrastructures.distances'),
+        :value => distances.join(', ')
+      }
+    end
+
+    {
+      :title => t('infrastructures.title'),
+      :collapsible => true,
+      :content => content
+    }
+  end
+
   def distances
     buffer = []
 
@@ -14,4 +58,5 @@ class InfrastructureDecorator < ApplicationDecorator
 
     buffer
   end
+
 end
