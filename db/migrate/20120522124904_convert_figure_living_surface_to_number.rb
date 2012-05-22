4bb53b28a8eb9700d843f30e124a2e046a506cce
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-05-22 15:20:55 +0200

    convert living_surface to integer oder so

---
+++ db/migrate/20120522124904_convert_figure_living_surface_to_number.rb
@@ -0,0 +1,15 @@
+class ConvertFigureLivingSurfaceToNumber < Mongoid::Migration
+
+  def self.up
+    RealEstate.all.each do |real_estate|
+      if real_estate.figure.present?
+        real_estate.figure.living_surface = real_estate.figure.living_surface
+        real_estate.figure.save!
+      end
+    end
+  end
+
+  def self.down
+  end
+
+end
