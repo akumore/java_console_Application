f75a95d2bb150f653bf065cba29c7c09c81a1219
Thomas Scholz <thomas.scholz@screenconcept.ch>
2012-05-22 15:51:03 +0200

    skip validations on migrating figures due to invalid figures

--- db/migrate/20120522124904_convert_figure_living_surface_to_number.rb
+++ db/migrate/20120522124904_convert_figure_living_surface_to_number.rb
@@ -4,7 +4,7 @@ class ConvertFigureLivingSurfaceToNumber < Mongoid::Migration
     RealEstate.all.each do |real_estate|
       if real_estate.figure.present?
         real_estate.figure.living_surface = real_estate.figure.living_surface
-        real_estate.figure.save!
+        real_estate.figure.save(:validate => false)
       end
     end
   end
