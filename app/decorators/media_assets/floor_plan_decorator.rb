804c02a8122e939031df5f151e05d5d619a10296
Sebastian de Castelberg <sebu@kpricorn.org>
2012-05-23 22:20:13 +0200

    rename north star to north arrow

--- app/decorators/media_assets/floor_plan_decorator.rb
+++ app/decorators/media_assets/floor_plan_decorator.rb
@@ -5,20 +5,23 @@ module MediaAssets
     decorates :floor_plan, :class => MediaAssets::FloorPlan
 
     def zoom_link
-      h.link_to I18n.t('.floorplan'), "#floorplan-zoomed-#{id}", :class => 'zoom-floorplan zoom-overlay'
+      h.link_to I18n.t('.floorplan'), "#floorplan-zoomed-#{id}",
+        :class => 'zoom-floorplan zoom-overlay'
     end
 
     def zoomed_div
-      h.content_tag(:div, :class => "floorplan-zoomed", :id => "floorplan-zoomed-#{id}") do
-        h.image_tag(floor_plan.file.url) + north_star_overlay
+      h.content_tag(:div, :class => "floorplan-zoomed",
+                    :id => "floorplan-zoomed-#{id}") do
+        h.image_tag(floor_plan.file.url) + north_arrow_overlay
       end
     end
 
-    def north_star_overlay
-      if real_estate.additional_description.orientation_degrees.present?
+    def north_arrow_overlay
+      additional_description = real_estate.additional_description
+      if additional_description.present? && additional_description.orientation_degrees.present?
         angle = real_estate.additional_description.orientation_degrees.to_i
         angle = angle - angle % 5
-        h.tag(:div, { :class => "north-star-overlay north-star-#{angle}" })
+        h.tag(:div, { :class => "north-arrow-overlay north-arrow-#{angle}" })
       end
     end
 
