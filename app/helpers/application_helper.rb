c82993c687b2697f07309e6286a18853cf3ef84d
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-06-20 10:38:47 +0200

    add subnavigation items to main-nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,19 @@ module ApplicationHelper
     seo_tags_html.html_safe
   end
 
+  def jobs_subnavigation
+    if jobs_page = Page.jobs_page
+      jobs_page.subnavigation.each do |title|
+        yield title
+      end
+    end
+  end
+
+  def company_subnavigation
+    if company_page = Page.company_page
+      company_page.subnavigation.each do |title|
+        yield title
+      end
+    end
+  end
 end
