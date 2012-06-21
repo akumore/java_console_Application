e0bb9d6b4c93e4599781fd0f8c5991656c1bc54e
Sebastian de Castelberg <sebu@kpricorn.org>
2012-06-21 11:47:54 +0200

    set mandatory editor on real estates in review if not set

---
+++ db/migrate/20120621092937_add_missing_editor_to_real_estate_in_review.rb
@@ -0,0 +1,16 @@
+class AddMissingEditorToRealEstateInReview < Mongoid::Migration
+  def self.up
+    RealEstate.where(:state => RealEstate::STATE_IN_REVIEW, :editor => nil) do |real_estate|
+      editor = Cms::User.where(:email => 'admin@screenconcept.ch').first
+      if editor.present?
+        real_estate.editor = editor
+        real_estate.save!
+      else
+        real_estate.reject_it!
+      end
+    end
+  end
+
+  def self.down
+  end
+end
