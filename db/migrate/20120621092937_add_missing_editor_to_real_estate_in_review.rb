4c463fed218ee4e6051238fd4217276b59e6ddb6
Sebastian de Castelberg <sebu@kpricorn.org>
2012-06-21 13:40:09 +0200

    check editor_id for nil

--- db/migrate/20120621092937_add_missing_editor_to_real_estate_in_review.rb
+++ db/migrate/20120621092937_add_missing_editor_to_real_estate_in_review.rb
@@ -1,6 +1,6 @@
 class AddMissingEditorToRealEstateInReview < Mongoid::Migration
   def self.up
-    RealEstate.where(:state => RealEstate::STATE_IN_REVIEW, :editor => nil) do |real_estate|
+    RealEstate.where(:state => RealEstate::STATE_IN_REVIEW, :editor_id => nil).each do |real_estate|
       editor = Cms::User.where(:email => 'admin@screenconcept.ch').first
       if editor.present?
         real_estate.editor = editor
