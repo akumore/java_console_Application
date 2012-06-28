4121f4f354111fef56547c978bd7e5ce3ce3db3d
Sebastian de Castelberg <sebu@kpricorn.org>
2012-06-28 17:04:58 +0200

    add reference project image uploader

---
+++ app/uploaders/reference_project_image_uploader.rb
@@ -0,0 +1,13 @@
+class ReferenceProjectImageUploader < BaseUploader
+
+  version :gallery do
+    process :resize_to_fill => [1000,500]
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    ExtensionWhiteList.new %w(jpg jpeg png)
+  end
+
+end
