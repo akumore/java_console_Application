09dabe684b04a2942b4e530292586238679942db
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-08-14 11:44:19 +0200

    add immediate availability date for dates in the past

--- spec/decorators/information_decorator_spec.rb
+++ spec/decorators/information_decorator_spec.rb
@@ -5,26 +5,49 @@ describe InformationDecorator do
 
   before :each do
     @real_estate = Fabricate(:published_real_estate,
-        :category => Fabricate(:category),
-        :offer => RealEstate::OFFER_FOR_RENT,
-        :information => Fabricate.build(:information,
-          :display_estimated_available_from => 'Mai 2012',
-          :has_balcony => true,
-          :has_swimming_pool => true,
-          :maximal_floor_loading => 140,
-          :freight_elevator_carrying_capacity => 150,
-        )
+      :category => Fabricate(:category),
+      :offer => RealEstate::OFFER_FOR_RENT,
+      :information => Fabricate.build(:information,
+        :display_estimated_available_from => 'Mai 2012',
+        :has_balcony => true,
+        :has_swimming_pool => true,
+        :maximal_floor_loading => 140,
+        :freight_elevator_carrying_capacity => 150,
       )
+    )
 
-      @information = InformationDecorator.new(@real_estate.information)
+    @information = InformationDecorator.new(@real_estate.information)
   end
 
-  it 'has the formatted availability date' do
-    @information.available_from_compact.should == 'Bezug ab Mai 2012'
+  context 'with an estimate avilability date' do
+    it 'has the formatted availability date' do
+      @information.available_from_compact.should == 'Bezug ab Mai 2012'
+    end
+
+    it 'has the pure availability date' do
+      @information.available_from.should == 'Mai 2012'
+    end
   end
 
-  it 'has the pure availability date' do
-    @information.available_from.should == 'Mai 2012'
+  context 'without an estimate availability date' do
+    before :each do
+      @real_estate.information.stub!(:display_estimated_available_from).and_return(nil)
+    end
+
+    it 'has the formatted availability date' do
+      @real_estate.information.stub!(:available_from).and_return(Date.parse('20.05.2030'))
+      @information.available_from_compact.should == 'Bezug ab 20.05.2030'
+    end
+
+    it 'has the pure availability date' do
+      @real_estate.information.stub!(:available_from).and_return(Date.parse('20.05.2030'))
+      @information.available_from.should == '20.05.2030'
+    end
+
+    it 'shows past availability dates as immediately available' do
+      @real_estate.information.stub!(:available_from).and_return(Date.parse('20.05.1986'))
+      @information.available_from.should == 'sofort'
+    end
   end
 
   it 'has a textual list of characteristics' do
