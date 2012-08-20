3fc9070001602e21b5331b4fe32c6a399f991182
Immanuel Häussermann <haeussermann@gmail.com>
2012-08-20 15:00:40 +0300

    remove TextHelper from migration

--- db/migrate/20120730154951_drop_property_name.rb
+++ db/migrate/20120730154951_drop_property_name.rb
@@ -1,5 +1,4 @@
 class DropPropertyName < Mongoid::Migration
-  extend ActionView::Helpers::TextHelper
 
   def self.up
     RealEstate.collection.update({}, {'$unset' => {:property_name => 1}}, :multi => true)
@@ -7,5 +6,6 @@ class DropPropertyName < Mongoid::Migration
 
   def self.down
   end
+
 end
 
