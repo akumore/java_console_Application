6897de528f45784e5bfecec01657fc569c4f410e
Sebastian de Castelberg <sebu@kpricorn.org>
2012-08-23 14:09:46 +0200

    call cache_key on handout rather than on real_estate

--- spec/models/real_estate_observer_spec.rb
+++ spec/models/real_estate_observer_spec.rb
@@ -40,7 +40,7 @@ describe RealEstateObserver do
 
       I18n.available_locales.each do |locale|
         %w(html pdf).each do |format|
-          observer.context.should_receive(:expire_page).with(observer.send :cache_key_for, real_estate, format, locale)
+          observer.context.should_receive(:expire_page).with(real_estate.handout.cache_key(format, locale))
         end
       end
 
