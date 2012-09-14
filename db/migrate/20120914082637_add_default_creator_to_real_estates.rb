306ab6cc32787f844cad28fe7b959a1704c17f57
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-09-14 14:07:30 +0200

    fix creator assignment in creation

--- db/migrate/20120914082637_add_default_creator_to_real_estates.rb
+++ db/migrate/20120914082637_add_default_creator_to_real_estates.rb
@@ -2,6 +2,11 @@ class AddDefaultCreatorToRealEstates < Mongoid::Migration
   def self.up
     RealEstate.all.each do |real_estate|
       real_estate.creator = real_estate.editor
+
+      if real_estate.creator.blank?
+        real_estate.creator = Cms::User.where(:email => 'admin@screenconcept.ch').first
+      end
+
       real_estate.save(:validate => false)
     end
   end
