ed62dd3dfd1682a3bf6382d1ee201f7c0c4a4663
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-09-18 15:17:27 +0200

    add news items controller spec

---
+++ spec/controllers/news_items_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe NewsItemsController, "handling GET /news_items.xml" do
+
+  before do
+    @request.env["HTTP_ACCEPT"] = "application/xml"
+    get :index, :format => :xml, :locale => :de
+  end
+
+  it "should render the action using XML" do
+    response.should be_success
+    response.should render_template('index')
+    response.headers["Content-Type"].should eql("application/xml; charset=utf-8")
+  end
+
+end
