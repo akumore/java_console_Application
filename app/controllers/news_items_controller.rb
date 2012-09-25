2ff31b48a427b710cf9f5b2854edadb1ecf76b49
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-09-25 13:18:51 +0200

    query news items individually per format

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,16 +1,13 @@
 class NewsItemsController < ApplicationController
 
-  respond_to do |format|
-    format.html
-    format.js
-    format.xml { render :layout => false }
-  end
-
   def index
     @offset = params[:offset].presence.to_i
-    @news_items = NewsItem.where(:locale => I18n.locale)
-                          .order([:date, :desc])
-                          .skip(@offset)
-                          .limit(NewsItem::PER_PAGE)
+    @news_items = NewsItem.where(:locale => I18n.locale).order([:date, :desc]).skip(@offset)
+
+    respond_to do |format|
+      format.html { @news_items = @news_items.limit(NewsItem::PER_PAGE) }
+      format.js   { @news_items = @news_items.limit(NewsItem::PER_PAGE) }
+      format.xml  { render :layout => false }
+    end
   end
 end
