bb3b0fae36e8bc92f687f7014fe4244e5068d855
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-10-10 09:53:38 +0200

    fix: always set backend language

--- app/controllers/cms/secured_controller.rb
+++ app/controllers/cms/secured_controller.rb
@@ -3,9 +3,14 @@ class Cms::SecuredController < ActionController::Base
   layout 'cms/application'
   respond_to :html
   helper_method :content_locale
+  before_filter :set_backend_language
   before_filter :authenticate_user!
 
   private
+
+  def set_backend_language
+    I18n.locale = :de
+  end
 
   def content_locale
     params[:content_locale].presence || I18n.default_locale
