457ba7838c363c3fb42f3183d06fe34e6d649f38
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-11-08 13:27:36 +0100

    rename target to account

--- lib/export/idx301/packager.rb
+++ lib/export/idx301/packager.rb
@@ -3,14 +3,14 @@ module Export
     class Packager < Logger::Application
       include Logging
 
-      def initialize(target)
-        super "#{target.name} Packager"
+      def initialize(account)
+        super "#{account.name} Packager"
         init_logging
 
         @time = Time.now
         @date_folder = @time.strftime("%Y_%m_%d")
         @time_folder = @time.strftime("%H_%M")
-        @target = target
+        @account = account
         create_folders
       end
 
@@ -24,7 +24,7 @@ module Export
 
       def package(real_estate)
         logger.info "Packaging."
-        Idx301::RealEstatePackage.new(real_estate, self, @target).save(unload_file)
+        Idx301::RealEstatePackage.new(real_estate, self, @account).save(unload_file)
       end
 
       def unload_file
@@ -32,7 +32,7 @@ module Export
       end
 
       def path
-        @path ||= File.join root_path, @date_folder, @time_folder, @target.name
+        @path ||= File.join root_path, @date_folder, @time_folder, @account.name
       end
 
       def doc_path
@@ -58,7 +58,7 @@ module Export
       private
 
       def create_folders
-        logger.info "Creating folder structure for #{@target.name}"
+        logger.info "Creating folder structure for #{@account.name}"
         FileUtils.mkdir_p path
         FileUtils.mkdir_p self.data_path
         FileUtils.mkdir_p self.image_path
--- lib/export/idx301/real_estate_package.rb
+++ lib/export/idx301/real_estate_package.rb
@@ -4,12 +4,12 @@ module Export
       include Logging
       attr_accessor :packager
 
-      def initialize(real_estate, packager, target)
-        super "#{target.name} RealEstate Package"
+      def initialize(real_estate, packager, account)
+        super "#{account.name} RealEstate Package"
         init_logging
 
         @packager = packager
-        @target = target
+        @account = account
         @real_estate = real_estate
         @images = []
         @videos = []
@@ -25,7 +25,7 @@ module Export
           add_image(floor_plan.file.gallery)
         end
 
-        if @target.video_support?
+        if @account.video_support?
           @real_estate.videos.each do |video|
             add_video(video.file)
           end
@@ -48,29 +48,29 @@ module Export
         ext   = File.extname(file.path)
         path  = file.path
         filename = "i_#{@real_estate.id}_#{@images.length + 1}#{ext}"
-        target_path = File.join(@packager.image_path, filename)
-        FileUtils.cp(path, target_path)
+        account_path = File.join(@packager.image_path, filename)
+        FileUtils.cp(path, account_path)
         @images << filename
       end
 
       def add_video(file)
         ext = File.extname(file.path)
         filename = "v_#{@real_estate.id}_#{@videos.length + 1}#{ext}"
-        target_path = File.join(@packager.movie_path, filename)
-        FileUtils.cp(file.path, target_path)
+        account_path = File.join(@packager.movie_path, filename)
+        FileUtils.cp(file.path, account_path)
         @videos << filename
       end
 
       def add_handout(handout)
         filename = "d_#{@real_estate.id}_#{@documents.length + 1}.pdf"
-        target_path = File.join(@packager.doc_path, filename)
+        account_path = File.join(@packager.doc_path, filename)
         handout_path = File.join Rails.root, 'public', handout.path
         if File.exists? handout_path
           logger.info "Adding cache file for handout #{handout.path}"
-          FileUtils.cp(handout_path, target_path)
+          FileUtils.cp(handout_path, account_path)
         else
           logger.info "Creating handout #{handout.path}"
-          handout.to_file(target_path)
+          handout.to_file(account_path)
         end
         @documents << filename
       end
@@ -78,15 +78,15 @@ module Export
       def add_document(file)
         ext = File.extname(file.path)
         filename = "d_#{@real_estate.id}_#{@documents.length + 1}#{ext}"
-        target_path = File.join(@packager.doc_path, filename)
-        FileUtils.cp(file.path, target_path)
+        account_path = File.join(@packager.doc_path, filename)
+        FileUtils.cp(file.path, account_path)
         @documents << filename
       end
 
       def save(unload_file)
         package_assets
-        logger.info "Writing unload.txt for #{@target.name}"
-        writer(unload_file).write Idx301::RealEstateDecorator.new(@real_estate, @target, asset_paths).to_a
+        logger.info "Writing unload.txt for #{@account.name}"
+        writer(unload_file).write Idx301::RealEstateDecorator.new(@real_estate, @account, asset_paths).to_a
       end
 
       private
