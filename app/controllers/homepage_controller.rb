b17478db8659821d2957b00ea841cfaffa6924c2
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-11-22 11:32:30 +0100

    increase homepage controller by published real estates

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,7 @@ class HomepageController < ApplicationController
   def index
     @projects_for_sale = ReferenceProjectDecorator.decorate ReferenceProject.for_sale.where(:locale => I18n.locale)
     @projects_for_rent = ReferenceProjectDecorator.decorate ReferenceProject.for_rent.where(:locale => I18n.locale)
+    @real_estates = RealEstateDecorator.decorate RealEstate.published.web_channel
   end
 
 end
