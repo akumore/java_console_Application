d52f4ac86d9ce82a96b2f13648f18e164d02a1fb
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-12-06 10:55:35 +0100

    wrap RealEstateDecorator around RealEstate in DashboardsController

--- app/controllers/cms/dashboards_controller.rb
+++ app/controllers/cms/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class Cms::DashboardsController < Cms::SecuredController
 
   respond_to :html
-  
+
   def show
     review_count = RealEstate.in_review.recently_updated.count
 
@@ -9,8 +9,8 @@ class Cms::DashboardsController < Cms::SecuredController
       flash.now[:notice] = t('cms.dashboards.real_estates_for_review', :count => review_count)
     end
 
-    @to_be_reviewed_real_estates = RealEstate.in_review.order([:updated_at, :desc])
-    @last_edited_real_estates = RealEstate.editing.order([:updated_at, :desc]).limit(5)
-    @last_published_real_estates = RealEstate.published.order([:updated_at, :desc]).limit(5)
+    @to_be_reviewed_real_estates = RealEstateDecorator.decorate(RealEstate.in_review.order([:updated_at, :desc]))
+    @last_edited_real_estates = RealEstateDecorator.decorate(RealEstate.editing.order([:updated_at, :desc]).limit(5))
+    @last_published_real_estates = RealEstateDecorator.decorate(RealEstate.published.order([:updated_at, :desc]).limit(5))
   end
 end
