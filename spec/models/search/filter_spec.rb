a6ab66c332eeb0641a6e95739c9f5d5f23b1382a
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-12-11 12:46:26 +0100

    make search filter #sortable? spec more robust

--- spec/models/search/filter_spec.rb
+++ spec/models/search/filter_spec.rb
@@ -173,28 +173,32 @@ describe Search::Filter do
   describe '#sortable?' do
     context 'when the utilization is parking' do
       it 'returns false' do
-        sf = Search::Filter.new(:utilization => Utilization::PARKING)
+        sf = Search::Filter.new
+        sf.stub!(:utilization).and_return(Utilization::PARKING)
         sf.sortable?.should be_false
       end
     end
 
     context 'when the utilization is living' do
       it 'returns true' do
-        sf = Search::Filter.new(:utilization => Utilization::LIVING)
+        sf = Search::Filter.new
+        sf.stub!(:utilization).and_return(Utilization::LIVING)
         sf.sortable?.should be_true
       end
     end
 
     context 'when the utilization is storing' do
       it 'returns true' do
-        sf = Search::Filter.new(:utilization => Utilization::WORKING)
+        sf = Search::Filter.new
+        sf.stub!(:utilization).and_return(Utilization::WORKING)
         sf.sortable?.should be_true
       end
     end
 
     context 'when the utilization is c' do
       it 'returns true' do
-        sf = Search::Filter.new(:utilization => Utilization::STORAGE)
+        sf = Search::Filter.new
+        sf.stub!(:utilization).and_return(Utilization::STORAGE)
         sf.sortable?.should be_true
       end
     end
