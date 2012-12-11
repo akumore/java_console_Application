df366676662f039387c2ff779fbf00f52b7a8bbf
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-12-11 16:27:15 +0100

    add accessible? helper which checks the cms_blacklist

--- app/models/field_access.rb
+++ app/models/field_access.rb
@@ -16,7 +16,7 @@ class FieldAccess
   end
 
   def key_for(model, field)
-    [@offer, @utilization, model.model_name, field].join('.')
+    [@offer, @utilization, model.class.name.underscore, field].join('.')
   end
 
   private
@@ -39,7 +39,54 @@ class FieldAccess
 
   def self.cms_blacklist
     %w(
-
+       rent.working.figure.rooms
+       rent.working.figure.rooms_estimate
+       rent.working.figure.living_surface
+       rent.working.figure.living_surface_estimate
+       rent.storing.figure.rooms
+       rent.storing.figure.rooms_estimate
+       rent.storing.figure.living_surface
+       rent.storing.figure.living_surface_estimate
+       rent.parking.figure.rooms
+       rent.parking.figure.rooms_estimate
+       rent.parking.figure.living_surface
+       rent.parking.figure.living_surface_estimate
+       sale.working.figure.rooms
+       sale.working.figure.rooms_estimate
+       sale.working.figure.living_surface
+       sale.working.figure.living_surface_estimate
+       sale.storing.figure.rooms
+       sale.storing.figure.rooms_estimate
+       sale.storing.figure.living_surface
+       sale.storing.figure.living_surface_estimate
+       sale.parking.figure.rooms
+       sale.parking.figure.rooms_estimate
+       sale.parking.figure.living_surface
+       sale.parking.figure.living_surface_estimate
+       rent.living.figure.usable_surface
+       rent.living.figure.usable_surface_estimate
+       rent.living.figure.storage_surface
+       rent.living.figure.ceiling_height
+       rent.storing.figure.usable_surface
+       rent.storing.figure.usable_surface_estimate
+       rent.storing.figure.storage_surface
+       rent.storing.figure.ceiling_height
+       ret.parking.figure.usable_surface
+       ret.parking.figure.usable_surface_estimate
+       ret.parking.figure.storage_surface
+       ret.parking.figure.ceiling_height
+       sale.living.figure.usable_surface
+       sale.living.figure.usable_surface_estimate
+       sale.living.figure.storage_surface
+       sale.living.figure.ceiling_height
+       sale.storing.figure.usable_surface
+       sale.storing.figure.usable_surface_estimate
+       sale.storing.figure.storage_surface
+       sale.storing.figure.ceiling_height
+       sale.parking.figure.usable_surface
+       sale.parking.figure.usable_surface_estimate
+       sale.parking.figure.storage_surface
+       sale.parking.figure.ceiling_height
     )
   end
 end
