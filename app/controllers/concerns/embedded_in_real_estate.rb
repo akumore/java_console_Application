3b7c41850156d678a134af56f56cd3258300844c
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-12-19 16:26:57 +0100

    use brackets

--- app/controllers/concerns/embedded_in_real_estate.rb
+++ app/controllers/concerns/embedded_in_real_estate.rb
@@ -8,11 +8,11 @@ module Concerns
       before_filter :save_editor, :only => [:update, :create]
     end
 
-    def success_message_for name
+    def success_message_for(name)
       "#{name.singularize.classify.constantize.model_name.human} erfolgreich gespeichert."
     end
 
-    def redirect_to_step name
+    def redirect_to_step(name)
       flash[:success] = success_message_for(controller_name)
       if @real_estate.send(name).present?
         redirect_to send("edit_cms_real_estate_#{name.singularize}_path", @real_estate)
