68c47c135c184c0e9857e839e261358e1f240aab
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-07 17:09:29 +0100

    add gallery photo model

---
+++ app/models/gallery_photo.rb
@@ -0,0 +1,21 @@
+class GalleryPhoto
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  default_scope asc(:position)
+
+  mount_uploader :image, GalleryPhotoUploader
+
+  field :title, :type => String
+  field :image, :type => String
+  field :position, :type => Integer
+
+  validates :title, :image, :presence => true
+
+  before_create :setup_position
+
+  private
+  def setup_position
+    self.position = GalleryPhoto.max(:position).to_i + 1
+  end
+end
