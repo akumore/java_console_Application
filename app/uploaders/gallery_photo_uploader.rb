c31e9249f6c04d870483c9c04da7c545d7f97f35
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-07 17:09:44 +0100

    add gallery photo uploader

---
+++ app/uploaders/gallery_photo_uploader.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class GalleryPhotoUploader < BaseUploader
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  version :contact do
+    process :resize_to_fill => [1000, 500]
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg png)
+  end
+end
