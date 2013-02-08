eedf9db845dd5db8e86add758583279bf99a8953
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-08 11:28:51 +0100

    add gallery photo model spec and fabricator

---
+++ spec/models/gallery_photo_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe GalleryPhoto do
+  describe 'initialize without any attributes' do
+    before :each do
+      @gallery_photo = GalleryPhoto.new
+    end
+
+    it 'does not pass validations' do
+      @gallery_photo.should_not be_valid
+    end
+
+    it 'requires a title' do
+      @gallery_photo.should have(1).error_on(:title)
+    end
+
+    it 'requires a image' do
+      @gallery_photo.should have(1).error_on(:image)
+    end
+
+    it 'has 2 errors' do
+      @gallery_photo.valid?
+      @gallery_photo.errors.should have(2).items
+    end
+  end
+end
