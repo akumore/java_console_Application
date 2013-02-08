bc05dbd7d87ca038e477e30187b52ccd839c85e7
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-08 13:49:48 +0100

    update reference project decorator for gallery photo

--- app/decorators/reference_project_decorator.rb
+++ app/decorators/reference_project_decorator.rb
@@ -2,15 +2,16 @@ class ReferenceProjectDecorator < ApplicationDecorator
   include Draper::LazyHelpers
 
   decorates :reference_project
+  decorates :gallery_photo
   decorates_association :real_estate
 
   def is_wide_content?
-    description.present? && description.length > 150
+    if model.has_attribute?(:descirption)
+      description.present? && description.length > 150
+    end
   end
 
   def title
     truncate(model.title, :length => 40)
   end
-
 end
-
