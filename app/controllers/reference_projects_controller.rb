e5839df8ce7a161ac44261354597db42d169b7c9
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-11 15:41:38 +0100

    update index view with reference projects

--- app/controllers/reference_projects_controller.rb
+++ app/controllers/reference_projects_controller.rb
@@ -2,6 +2,6 @@ class ReferenceProjectsController < ApplicationController
 
   def index
     @gallery_photos = ReferenceProjectDecorator.decorate GalleryPhoto.all
-    @reference_projects = ReferenceProjectDecorator.decorate ReferenceProject.all
+    @reference_projects = ReferenceProjectDecorator.decorate ReferenceProject.where(:locale => I18n.locale)
   end
 end
