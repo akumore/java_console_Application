3a0b9b2bb45135b03da0bba4c721793c571a10dd
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-13 10:41:57 +0100

    add format for reference projects

--- app/controllers/reference_projects_controller.rb
+++ app/controllers/reference_projects_controller.rb
@@ -3,5 +3,10 @@ class ReferenceProjectsController < ApplicationController
   def index
     @gallery_photos = ReferenceProjectDecorator.decorate GalleryPhoto.all
     @reference_projects = ReferenceProjectDecorator.decorate ReferenceProject.where(:locale => I18n.locale)
+
+    respond_to do |format|
+      format.html { @reference_projects = @reference_projects.limit(4) }
+      format.js   { @reference_projects = @reference_projects.all }
+    end
   end
 end
