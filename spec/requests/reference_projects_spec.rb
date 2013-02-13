4cb378d900a32d9db56aed592ab232111b33a57b
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-02-13 11:34:30 +0100

    split and tidy up specs for gallery photos and reference projects

--- spec/requests/reference_projects_spec.rb
+++ spec/requests/reference_projects_spec.rb
@@ -1,54 +1,55 @@
 # encoding: utf-8
 
-require "spec_helper"
+require 'spec_helper'
 
 describe ReferenceProject do
   monkey_patch_default_url_options
 
-  let :first do
-    Fabricate(:gallery_photo,
-              :title => 'Titel 1',
-              :position => 1
-             )
-  end
+  describe 'shows the reference projects list' do
+    before :each do
+      visit reference_projects_path
+    end
 
-  let :second do
-    Fabricate(:gallery_photo,
-              :title => 'Titel 2',
-              :position => 2
-             )
-  end
-
-  let :third do
-    Fabricate(:gallery_photo,
-              :title => 'Titel 3',
-              :position => 3
-             )
-  end
-
-  describe "Gallery Photo Slider" do
-    describe 'with existing gallery photos' do
+    context 'with 5 existing reference projects' do
       before do
-        @gallery_photos = [first, second, third]
+        5.times do
+          Fabricate(:reference_project, :offer => RealEstate::OFFER_FOR_SALE, :utilization => RealEstate::UTILIZATION_PRIVATE)
+        end
         visit reference_projects_path
       end
 
-      it "shows the gallery photos slider" do
-        page.should have_selector('.gallery-photos-slider', :count => 1)
+      it 'shows the projects container' do
+        page.should have_css('.reference-projects-container')
       end
 
-      it "has 3 items in the slider" do
-        page.should have_selector('.gallery-photos-slider li', :count => 3)
+      context 'without clicking on load more button' do
+        it 'has 4 projects' do
+          page.should have_css('.reference-projects-container .reference-project', :count => 4)
+        end
+      end
+
+      context 'with clicking on load more button', :js => true do
+        before do
+          click_link 'Mehr anzeigen'
+        end
+
+        it 'has 5 projects' do
+          page.should have_css('.reference-projects-container .reference-project', :count => 5)
+        end
       end
     end
 
-    describe 'without any gallery photos' do
-      before do
-        visit reference_projects_path
+    context 'with non existing reference projects' do
+      it 'shows no projects container' do
+        page.should_not have_css('.reference-projects-container')
       end
 
-      it "shows no gallery photos slider" do
-        page.should_not have_selector('.gallery-photos-slider')
+      it 'has no projects' do
+        page.should_not have_css('.reference-projects-container .reference-project')
+      end
+
+      it 'does not show the load more link' do
+        page.should_not have_css('.reference-projects-container .load-more')
       end
     end
   end
