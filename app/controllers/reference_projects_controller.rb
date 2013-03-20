72694cc0d732d4f17df010b1d91a7458c083f3a4
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-03-20 16:36:49 +0100

    filter reference projects by section

--- app/controllers/reference_projects_controller.rb
+++ app/controllers/reference_projects_controller.rb
@@ -1,12 +1,25 @@
 class ReferenceProjectsController < ApplicationController
 
+  before_filter :set_search_filter
+
   def index
     @gallery_photos = GalleryPhotoDecorator.decorate GalleryPhoto.all
-    @reference_projects = ReferenceProjectDecorator.decorate ReferenceProject.where(:locale => I18n.locale)
+    @reference_projects = get_filtered_reference_projects(@search_filter)
 
     respond_to do |format|
       format.html { @reference_projects = @reference_projects.limit(4) }
-      format.js   { @reference_projects = @reference_projects.all }
+      format.js { @reference_projects = @reference_projects.all }
     end
+  end
+
+  private
+
+  def set_search_filter
+    filter_params = (params[:search_filter] || {}).reverse_merge(:section => params[:section])
+    @search_filter = Search::ReferenceProjectsFilter.new(filter_params)
+  end
+
+  def get_filtered_reference_projects(search_filter)
+    ReferenceProject.where( :locale => I18n.locale, :section => search_filter.section )
   end
 end
