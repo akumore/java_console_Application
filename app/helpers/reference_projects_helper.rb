8282b6d4ddc3630130429cb0b97fa3cb05719610
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-03-20 16:36:00 +0100

    fix spelling

--- app/helpers/reference_projects_helper.rb
+++ app/helpers/reference_projects_helper.rb
@@ -2,7 +2,7 @@ module ReferenceProjectsHelper
 
   def reference_project_sections
     [
-      [t("reference_projects.section.residental_building"), ReferenceProjectSection::RESIDENTIAL_BUILDING],
+      [t("reference_projects.section.residential_building"), ReferenceProjectSection::RESIDENTIAL_BUILDING],
       [t("reference_projects.section.business_building"), ReferenceProjectSection::BUSINESS_BUILDING],
       [t("reference_projects.section.public_building"), ReferenceProjectSection::PUBLIC_BUILDING],
       [t("reference_projects.section.rebuilding"), ReferenceProjectSection::REBUILDING]
