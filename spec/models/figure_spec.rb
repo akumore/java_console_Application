74214fd993cf5a1f1e90ee80c3730b93f0b731c1
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-03-26 11:46:17 +0100

    fix figure spec

--- spec/models/figure_spec.rb
+++ spec/models/figure_spec.rb
@@ -35,7 +35,7 @@ describe Figure do
       end
 
       it 'requires a number for floor' do
-        @figure.should have(1).error_on(:floor)
+        @figure.should have(2).error_on(:floor)
       end
 
       it 'requires a number for living_surface' do
