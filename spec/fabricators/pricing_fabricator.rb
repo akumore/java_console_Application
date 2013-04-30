2f422da29c8adfb82089157ef4ace6647e36fae6
Sebastian de Castelberg <sebu@kpricorn.org>
2013-04-30 15:48:38 +0200

    rename for_rent_extra to new additional_costs in fabricator

--- spec/fabricators/pricing_fabricator.rb
+++ spec/fabricators/pricing_fabricator.rb
@@ -1,7 +1,8 @@
 Fabricator(:pricing) do
   for_rent_netto 1520
-  for_rent_extra 100
   for_sale 1300000
+  additional_costs 100
+  estimate 10
   price_unit 'monthly'
   inside_parking 140
   outside_parking 120
@@ -16,11 +17,12 @@ end
 
 Fabricator(:pricing_for_rent, :class_name => :pricing) do
   for_rent_netto 1999
-  for_rent_extra 99
+  additional_costs 99
   price_unit "monthly"
 end
 
 Fabricator(:pricing_for_sale, :class_name => :pricing) do
   for_sale 1300000
+  additional_costs 99
   price_unit 'sell'
 end
