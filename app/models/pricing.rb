2c8a701344fa852a80771a0cde0e218d5e23a516
Phil Schilter <phil.schilter@gmail.com>
2013-05-01 16:58:30 +0200

    add missing condition for year_m2 PriceUnit

--- app/models/pricing.rb
+++ app/models/pricing.rb
@@ -57,8 +57,8 @@ class Pricing
                           },
                          :if => :parking?
 
-  validates :for_rent_netto_monthly,   :presence => true, :if => :price_unit_is_per_square_meter_per_year?
-  validates :additional_costs_monthly, :presence => true, :if => :price_unit_is_per_square_meter_per_year?, :unless => :parking?
+  validates :for_rent_netto_monthly,   :presence => true, :if => :for_rent? && :price_unit_is_per_square_meter_per_year?
+  validates :additional_costs_monthly, :presence => true, :if => :for_rent? && :price_unit_is_per_square_meter_per_year?, :unless => :parking?
 
   delegate :for_sale?,
            :for_rent?,
