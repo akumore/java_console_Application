69737e81e7d3455ca17ea3536dedadd869c9b4ee
Phil Schilter <phil.schilter@gmail.com>
2013-05-21 13:07:17 +0200

    update reference project sections

--- app/helpers/reference_projects_helper.rb
+++ app/helpers/reference_projects_helper.rb
@@ -3,8 +3,10 @@ module ReferenceProjectsHelper
   def reference_project_sections
     [
       [t("reference_projects.section.residential_building"), ReferenceProjectSection::RESIDENTIAL_BUILDING],
+      [t("reference_projects.section.residential_commercial_building"), ReferenceProjectSection::RESIDENTIAL_COMMERCIAL_BUILDING],
       [t("reference_projects.section.business_building"), ReferenceProjectSection::BUSINESS_BUILDING],
-      [t("reference_projects.section.public_building"), ReferenceProjectSection::PUBLIC_BUILDING],
+      [t("reference_projects.section.trade_industrial_building"), ReferenceProjectSection::TRADE_INDUSTRIAL_BUILDING],
+      [t("reference_projects.section.special_building"), ReferenceProjectSection::SPECIAL_BUILDING],
       [t("reference_projects.section.rebuilding"), ReferenceProjectSection::REBUILDING]
     ]
   end
