1ab0936282b4955eaeabcc738fc8c41558a25679
Phil Schilter <phil.schilter@gmail.com>
2013-06-17 16:39:14 +0200

    add HandoutOrdersController

---
+++ app/controllers/handout_orders_controller.rb
@@ -0,0 +1,17 @@
+class HandoutOrdersController < ApplicationController
+
+  def new
+    @handout_order = HandoutOrder.new
+  end
+
+  def create
+    @handout_order = HandoutOrder.new(params[:handout_order])
+    @real_estate = RealEstate.find(params[:real_estate_id])
+    if @handout_order.save
+      HandoutOrderMailer.handout_order_notification(@handout_order, @real_estate).deliver
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+end
