03fe3f29b487fc18c39ec6e771689c7d41703bc7
Phil Schilter <phil.schilter@gmail.com>
2013-06-17 16:34:49 +0200

    add HandoutOrder model

---
+++ app/models/handout_order.rb
@@ -0,0 +1,22 @@
+class HandoutOrder
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :company,   :type => String
+  field :firstname, :type => String
+  field :lastname,  :type => String
+  field :email,     :type => String
+  field :phone,     :type => String
+  field :street,    :type => String
+  field :zipcode,   :type => String
+  field :city,      :type => String
+
+  validates :firstname,
+            :lastname,
+            :email,
+            :phone,
+            :street,
+            :zipcode,
+            :city,
+            :presence => true
+end
