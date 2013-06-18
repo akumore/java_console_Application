9d0cf26d2786d57f40822f3b0ad56d5917891867
Phil Schilter <phil.schilter@gmail.com>
2013-06-18 10:59:28 +0200

    increase HandoutOrdersController by decorated RealEstate

--- app/controllers/handout_orders_controller.rb
+++ app/controllers/handout_orders_controller.rb
@@ -1,12 +1,13 @@
 class HandoutOrdersController < ApplicationController
 
   def new
+    @real_estate = RealEstateDecorator.find(params[:real_estate_id])
     @handout_order = HandoutOrder.new
   end
 
   def create
     @handout_order = HandoutOrder.new(params[:handout_order])
-    @real_estate = RealEstate.find(params[:real_estate_id])
+    @real_estate = RealEstateDecorator.find(params[:real_estate_id])
     if @handout_order.save
       HandoutOrderMailer.handout_order_notification(@handout_order, @real_estate).deliver
       render 'show'
