322b349069bd2355269862018a1e73483cd5533b
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2013-06-18 14:02:35 +0200

    add microsite model with available microsites

---
+++ app/models/microsite.rb
@@ -0,0 +1,25 @@
+module Microsite
+
+  FELDPARK  = 'feldpark'
+  BUENZPARK = 'buenzpark'
+  GARTENSTADT = 'gartenstadt'
+
+  def self.all
+    [GARTENSTADT, FELDPARK, BUENZPARK]
+  end
+
+  module Accessors
+    def feldpark?
+      microsite == FELDPARK
+    end
+
+    def buenzpark?
+      microsite == BUENZPARK
+    end
+
+    def gartenstadt?
+      microsite == GARTENSTADT
+    end
+  end
+
+end
