38a04438475521731e1f4c58d2ded32a4bc19e5b
Phil Schilter <phil.schilter@gmail.com>
2013-08-08 13:00:50 +0200

    Use I18n class

--- lib/google_analytics_category_translator.rb
+++ lib/google_analytics_category_translator.rb
@@ -1,6 +1,6 @@
 module GoogleAnalyticsCategoryTranslator
 
   def translate_category(real_estate)
-    "#{t("real_estates.search_filter.#{real_estate.offer}", locale: :de)} #{t("real_estates.search_filter.#{real_estate.utilization}", locale: :de)}"
+    "#{I18n.t("real_estates.search_filter.#{real_estate.offer}", locale: :de)} #{I18n.t("real_estates.search_filter.#{real_estate.utilization}", locale: :de)}"
   end
 end
