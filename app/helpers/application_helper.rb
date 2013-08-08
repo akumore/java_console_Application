4a817a16eef7b96ce88312c4db90e3e6c65f7dbf
Phil Schilter <phil.schilter@gmail.com>
2013-08-08 16:01:35 +0200

    remove markdown helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@
 
 module ApplicationHelper
 
-  def markdown string
-    RDiscount.new(string).to_html.html_safe
-  end
-
   def path_to_url(path)
       request.protocol + request.host_with_port + path
   end
