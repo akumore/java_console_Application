69de5534fd1c3133a76e40d78f28ff90290e075f
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2013-09-03 11:30:46 +0200

    correct floor_plan asset validation

--- app/controllers/cms/floor_plans_controller.rb
+++ app/controllers/cms/floor_plans_controller.rb
@@ -1,8 +1,8 @@
 class Cms::FloorPlansController < Cms::SecuredController
   include Concerns::EmbeddedInRealEstate
 
-  load_resource :class=>MediaAssets::FloorPlan, :through => :real_estate
-  authorize_resource :class=>MediaAssets::FloorPlan, :through => :real_estate, :except => :show
+  load_resource :class => MediaAssets::FloorPlan, :through => :real_estate
+  authorize_resource :class => MediaAssets::FloorPlan, :through => :real_estate, :except => :show
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to cms_real_estate_media_assets_path(@real_estate), :alert => exception.message
@@ -19,20 +19,24 @@ class Cms::FloorPlansController < Cms::SecuredController
 
   def create
     @floor_plan = @real_estate.floor_plans.create(params[:floor_plan])
-    if @real_estate.valid?
-      redirect_to edit_cms_real_estate_floor_plan_path(@real_estate, @floor_plan)
+    if @floor_plan.valid?
+      render :edit
     else
-      render 'new'
+      render :new
     end
   end
 
   def update
     @floor_plan.update_attributes(params[:floor_plan])
-    redirect_to edit_cms_real_estate_floor_plan_path(@real_estate, @floor_plan)
+    if @floor_plan.valid? && params[:floor_plan][:crop_x].blank?
+      redirect_to [:cms, @real_estate, :media_assets]
+    else
+      render :edit
+    end
   end
 
   def destroy
     @floor_plan.destroy
-    redirect_to cms_real_estate_media_assets_path
+    redirect_to [:cms, @real_estate, :media_assets]
   end
 end
