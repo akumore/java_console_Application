ed19d8ed82dbdc930c1653fc854a98ea626f39a0
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2013-09-24 14:21:53 +0200

    spec storing real estates in microsite api

--- spec/lib/microsite/group_real_estate_spec.rb
+++ spec/lib/microsite/group_real_estate_spec.rb
@@ -11,6 +11,7 @@ module Microsite
         :private_utilization? => true,
         :commercial_utilization? => false,
         :category_label => nil,
+        :storing? => false
       )
     end
 
@@ -18,10 +19,18 @@ module Microsite
       stub(:commercial_utilization? => true)
     end
 
+    let :storing_real_estate do
+      stub(
+        :category_label => 'Disponibel',
+        :commercial_utilization? => false,
+        :storing? => true
+      )
+    end
+
     context 'as uncategorized real estate for private use' do
       it 'returns the value of \'rooms\' attribute as grouping key' do
         private_real_estate.stub(:figure => stub(:rooms => '3.5'))
-        GroupRealEstates.get_group(private_real_estate)[:label].should == '3.5-Zimmer'
+        GroupRealEstates.get_group(private_real_estate)[:label].should == '3.5 Zimmer'
       end
 
       context 'rooms are set to \'0\'' do
@@ -45,6 +54,12 @@ module Microsite
       end
     end
 
+    context 'as storing building' do
+      it 'returns the category name as grouping key' do
+        GroupRealEstates.get_group(storing_real_estate)[:label].should == 'Disponibel'
+      end
+    end
+
     context 'grouping' do
       it 'sorts the objects in the order: number of rooms, Loft, Dienstleistungsflächen' do
         sort_keys = []
@@ -55,9 +70,10 @@ module Microsite
         private_real_estate.stub(:figure => stub(:rooms => '3.5'))
         sort_keys << GroupRealEstates.get_group(private_real_estate)[:sort_key]
         private_real_estate.stub(:category_label => 'Loft')
-        sort_keys << GroupRealEstates.get_group(private_real_estate)[:sort_key]
         sort_keys << GroupRealEstates.get_group(commercial_real_estate)[:sort_key]
-        sort_keys.sort.should == [ '1.5', '3.5', 'A', 'B', 'C' ]
+        sort_keys << GroupRealEstates.get_group(storing_real_estate)[:sort_key]
+        sort_keys << GroupRealEstates.get_group(private_real_estate)[:sort_key]
+        sort_keys.sort.should == [ '1.5', '3.5', 'A', 'B', 'C', 'D' ]
       end
     end
 
