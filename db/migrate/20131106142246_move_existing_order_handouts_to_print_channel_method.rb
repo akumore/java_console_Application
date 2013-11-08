6b084aea52884bc9668486f40429c07888f57204
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2013-11-08 16:43:56 +0100

    fix print channel method migration

--- db/migrate/20131106142246_move_existing_order_handouts_to_print_channel_method.rb
+++ db/migrate/20131106142246_move_existing_order_handouts_to_print_channel_method.rb
@@ -7,16 +7,25 @@ class MoveExistingOrderHandoutsToPrintChannelMethod < Mongoid::Migration
   def self.up
     RealEstate.all.each do |real_estate|
       if real_estate.order_handout
-        real_estate.update_attribute(:print_channel_method, RealEstate::PRINT_CHANNEL_METHOD_ORDER)
+        real_estate.channels << RealEstate::PRINT_CHANNEL
+        real_estate.print_channel_method = RealEstate::PRINT_CHANNEL_METHOD_ORDER
       else
-        real_estate.update_attribute(:print_channel_method, RealEstate::PRINT_CHANNEL_METHOD_PDF_DOWNLOAD)
+        if real_estate.channels.include? RealEstate::PRINT_CHANNEL
+          real_estate.print_channel_method = RealEstate::PRINT_CHANNEL_METHOD_PDF_DOWNLOAD
+        end
       end
+      real_estate.save(:validate => false)
     end
   end
 
   def self.down
     RealEstate.all.each do |real_estate|
-      real_estate.update_attribute(:print_channel_method, '')
+      if RealEstate::PRINT_CHANNEL_METHOD_ORDER == real_estate.print_channel_method
+        real_estate.channels.delete(RealEstate::PRINT_CHANNEL)
+        real_estate.order_handout = true
+      end
+      real_estate.print_channel_method = ''
+      real_estate.save(:validate => false)
     end
   end
 end
