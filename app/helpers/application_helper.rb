3487277ea9b4f21c64279ed06de1fbbdaefec24c
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2013-12-13 10:58:31 +0100

    avoid image caching in IE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,4 +47,8 @@ module ApplicationHelper
       end
     end
   end
+
+  def non_caching_image_tag(source, options = {})
+    image_tag "#{source}?id=#{Random.new.rand(1_000..10_000-1)}", options
+  end
 end
