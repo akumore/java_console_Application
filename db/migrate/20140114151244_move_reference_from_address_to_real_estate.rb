b62279684362a35e55bcb2673d2423d3524f1f0f
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2014-01-16 16:20:39 +0100

    make migration of moving reference from address to real estate work

--- db/migrate/20140114151244_move_reference_from_address_to_real_estate.rb
+++ db/migrate/20140114151244_move_reference_from_address_to_real_estate.rb
@@ -1,20 +1,9 @@
 class MoveReferenceFromAddressToRealEstate < Mongoid::Migration
   def self.up
-    RealEstate.all.each do |real_estate|
-      if real_estate.export_to_real_estate_portal? && real_estate.address.present?
-        reference = real_estate.address.reference
-      else
-        reference = Reference.new
-      end
-      real_estate.reference = reference
-      real_estate.save(validate: false)
-    end
+    RealEstate.collection.update({}, { '$rename' => { "address.reference" => "reference"} }, :multi => true)  
   end
 
   def self.down
-    RealEstate.all.each do |real_estate|
-      real_estate.reference = nil
-      real_estate.save(validate: false)
-    end
+    RealEstate.collection.update({}, { '$rename' => { "reference" => "address.reference" } }, :multi => true)
   end
 end
