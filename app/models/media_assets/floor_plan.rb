eed3e2175575f8944c46ca666d26570a1ad7dd30
Phil Schilter <phil.schilter@gmail.com>
2014-02-10 16:03:00 +0100

    add orientation degrees field to floor plan

--- app/models/media_assets/floor_plan.rb
+++ app/models/media_assets/floor_plan.rb
@@ -2,6 +2,8 @@ module MediaAssets
   class FloorPlan < ImageBase
     mount_uploader :file, MediaAssets::FloorPlanUploader
 
+    field :orientation_degrees, :type => Integer
+
     private
     def setup_position
       self.position ||= real_estate.floor_plans.max(:position).to_i + 1
