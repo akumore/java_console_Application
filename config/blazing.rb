236b0cc29296a8e541b455900ab93fabb679398c
Phil Schilter <phil.schilter@gmail.com>
2014-03-14 14:52:11 +0100

    update blazing config

--- config/blazing.rb
+++ config/blazing.rb
@@ -1,13 +1,4 @@
-#
-# Blazing Configuration File
-#
-#
-repository 'git@github.com:screenconcept/alflred_mueller.git'
-
-target :production, 'alfred_mueller@scrcpt2.nine.ch:/home/usr/alfred_mueller/public_html', :url => 'http://production.alfredmueller.screenconcept.ch', :rails_env => 'production'
-target :staging, 'amstaging@scrcpt2.nine.ch:/home/usr/amstaging/public_html', :url => 'http://staging.alfredmueller.screenconcept.ch', :rails_env => 'staging'
-
+target :production, 'www-data@.screenconcept.ch:/home/www-data/apps/alfredmueller_web_production', rails_env: 'production'
+target :staging, 'www-data@.screenconcept.ch:/home/www-data/apps/alfredmueller_web_staging', rails_env: 'staging'
 rvm :rvmrc
-rvm_scripts '/opt/rvm/scripts/rvm'
-
 rake 'post_deploy'
