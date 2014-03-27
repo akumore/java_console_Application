f1e4937014822c3aef557a9484ff6c15f51df742
Phil Schilter <phil.schilter@gmail.com>
2014-03-27 15:48:38 +0100

    update blazing config

--- config/blazing.rb
+++ config/blazing.rb
@@ -1,4 +1,5 @@
-target :production, 'www-data@.screenconcept.ch:/home/www-data/apps/alfredmueller_web_production', rails_env: 'production'
-target :staging, 'www-data@.screenconcept.ch:/home/www-data/apps/alfredmueller_web_staging', rails_env: 'staging'
+target :production, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_production', rails_env: 'production'
+target :staging, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_staging', rails_env: 'staging'
+
 rvm :rvmrc
 rake 'post_deploy'
