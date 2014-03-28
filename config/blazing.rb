138af215871af4855292b629c31ba787ad79202a
Flavio Pellanda <user@ntnb0102.victor-hotz.local>
2014-03-28 16:00:29 +0100

    Added missing dash

--- config/blazing.rb
+++ config/blazing.rb
@@ -1,5 +1,5 @@
 target :production, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_production', rails_env: 'production'
 target :staging, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_staging', rails_env: 'staging'
 
-env_scripts ENV['HOME'] + 'bin/rbenv.sh'
+env_scripts '/home/www-data/bin/rbenv.sh'
 rake 'post_deploy'
