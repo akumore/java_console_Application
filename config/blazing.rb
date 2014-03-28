8a8832549f7cf1b6d2649eddd16284f3fcc36b44
Flavio Pellanda <user@ntnb0102.victor-hotz.local>
2014-03-28 15:03:29 +0100

    Remove rvm from blazing config

--- config/blazing.rb
+++ config/blazing.rb
@@ -1,5 +1,4 @@
 target :production, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_production', rails_env: 'production'
 target :staging, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_staging', rails_env: 'staging'
 
-rvm :rvmrc
 rake 'post_deploy'
