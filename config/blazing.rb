a7d0adea1b40cf1c701b27aee109ff792207eaa2
Flavio Pellanda <user@ntnb0102.victor-hotz.local>
2014-03-28 15:38:19 +0100

    Define rbenv environment script for blazing

--- config/blazing.rb
+++ config/blazing.rb
@@ -1,4 +1,5 @@
 target :production, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_production', rails_env: 'production'
 target :staging, 'www-data@c3.screenconcept.ch:/home/www-data/apps/alfredmueller_web_staging', rails_env: 'staging'
 
+env_scripts ENV['HOME'] + 'bin/rbenv.sh'
 rake 'post_deploy'
