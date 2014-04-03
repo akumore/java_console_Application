605a4158b3e9fa85978ff858dc44bab773f9f5cd
Phil Schilter <phil.schilter@gmail.com>
2014-04-03 11:22:53 +0200

    update staging mail interceptor recipients

--- lib/staging_mail_interceptor.rb
+++ lib/staging_mail_interceptor.rb
@@ -1,7 +1,7 @@
 class StagingMailInterceptor
   def self.delivering_email(message)
     message.subject = "TESTMAIL: #{message.subject}"
-    message.to = %w(guelay.tatlici@alfred-mueller.ch christina.schweizer@alfred-mueller.ch)
-    message.cc = %w(melinda.lini@screenconcept.ch immanuel.haeussermann@screenconcept.ch thomas.scholz@screenconcept.ch martin.wulff@screenconcept.ch)
+    message.to = %w(Janine.Wyss@alfred-mueller.ch)
+    message.cc = %w(melinda.lini@screenconcept.ch flavio.pellanda@screenconcept.ch noelle.rosenberg@screenconcept.ch philipp.schilter@screenconcept.ch)
   end
 end
