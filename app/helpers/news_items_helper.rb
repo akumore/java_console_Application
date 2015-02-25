aa2976d67199ece599db198e456cd585d2bc30a1
Phil Schilter <phil.schilter@gmail.com>
2015-02-25 16:41:58 +0100

    sort news items by year

--- app/helpers/news_items_helper.rb
+++ app/helpers/news_items_helper.rb
@@ -1,8 +1,6 @@
 module NewsItemsHelper
 
-  def load_more_link current_offset
-    link_to t('news_items.index.load_more'), 
-            news_items_path(:offset => current_offset + NewsItem::PER_PAGE), 
-            :remote => true
+  def heyhey
+    @news_items_by_year.keys.map { |year| [year] }
   end
 end
--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -3,18 +3,18 @@ class NewsItem
   include Mongoid::Timestamps
   include Mongoid::MultiParameterAttributes
 
-  accepts_nested_attributes_for :images, :allow_destroy => true, :reject_if => :all_blank
-  embeds_many :images, :class_name=>"NewsItemImage", cascade_callbacks: true
+  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :all_blank
+  embeds_many :images, class_name: "NewsItemImage", cascade_callbacks: true
 
-  accepts_nested_attributes_for :documents, :allow_destroy => true, :reject_if => :all_blank
-  embeds_many :documents, :class_name=>"NewsItemDocument", cascade_callbacks: true
+  accepts_nested_attributes_for :documents, allow_destroy: true, reject_if: :all_blank
+  embeds_many :documents, class_name: "NewsItemDocument", cascade_callbacks: true
 
   PER_PAGE = 6
 
-  field :title, :type => String
-  field :content, :type => String
-  field :date, :type => Date
-  field :locale, :type => String, :default => 'de'
+  field :title, type: String
+  field :content, type: String
+  field :date, type: Date
+  field :locale, type: String, default: 'de'
 
-  validates :title, :content, :date, :presence => true
+  validates :title, :content, :date, presence: true
 end
