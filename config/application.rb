3004c08a6fafac60a7bc147dffe23838f6cebc22
Noelle Rosenberg <noelle.rosenberg@screenconcept.ch>
2015-04-01 09:53:28 +0200

    hide deprecation warning for not enforcing available locales

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module AlfredMueller
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    I18n.config.enforce_available_locales = true
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:de, :fr, :it, :en]
     config.i18n.default_locale = :de
