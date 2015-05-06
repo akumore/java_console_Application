da2466d13e25c85c9b117dddfc721a14d4e113cf
Phil Schilter <phil.schilter@gmail.com>
2015-05-06 17:52:07 +0200

    localize news item model

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -6,18 +6,28 @@ class NewsItem
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :all_blank
   embeds_many :images, class_name: "NewsItemImage", cascade_callbacks: true
 
-  accepts_nested_attributes_for :documents, allow_destroy: true, reject_if: :all_blank
-  embeds_many :documents, class_name: "NewsItemDocument", cascade_callbacks: true
+  I18n.available_locales.each do |locale|
+    accepts_nested_attributes_for :"documents_#{locale}", allow_destroy: true, reject_if: :all_blank
+    embeds_many :"documents_#{locale}", class_name: "NewsItemDocument", cascade_callbacks: true
+  end
 
   scope :published, lambda { where(published: true) }
 
   PER_PAGE = 6
 
-  field :title, type: String
-  field :content, type: String
+  field :title, type: String, localize: true
+  field :content, type: String, localize: true
   field :date, type: Date
   field :locale, type: String, default: 'de'
-  field :published, type: Boolean, default: true
+  field :published, type: Boolean, default: true, localize: true
 
   validates :title, :content, :date, presence: true
+
+  def documents
+    send :"documents_#{I18n.locale}"
+  end
+
+  def documents=(*args)
+    send :"documents_#{I18n.locale}=", *args
+  end
 end
