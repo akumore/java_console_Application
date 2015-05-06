6e673320661963919443d8b948cc82c2532b629b
Phil Schilter <phil.schilter@gmail.com>
2015-05-06 17:50:33 +0200

    add migration for localized fields

---
+++ db/migrate/20150506105200_translate_news_items.rb
@@ -0,0 +1,17 @@
+class TranslateNewsItems < Mongoid::Migration
+  def self.up
+    NewsItem.collection.update({}, { '$rename' => { "documents" => "documents_de"} }, :multi => true)
+
+    NewsItem.all.each do |item|
+      I18n.with_locale item.locale do
+        item.title_translations     = { I18n.locale.to_s => item.title_translations }
+        item.content_translations   = { I18n.locale.to_s => item.content_translations }
+        item.published_translations = { I18n.locale.to_s => item.published_translations }
+        item.save!
+      end
+    end
+  end
+
+  def self.down
+  end
+end
