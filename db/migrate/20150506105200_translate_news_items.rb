263357d3d98d6c6dfc58fbde17e49958d9085891
Phil Schilter <phil.schilter@gmail.com>
2015-05-13 10:06:45 +0200

    fix news items migration

--- db/migrate/20150506105200_translate_news_items.rb
+++ db/migrate/20150506105200_translate_news_items.rb
@@ -1,12 +1,18 @@
 class TranslateNewsItems < Mongoid::Migration
   def self.up
+    @locales = I18n.available_locales
+
     NewsItem.all.each do |item|
       I18n.with_locale item.locale do
-        item.title_translations     = { I18n.locale.to_s => item.title_translations }
-        item.content_translations   = { I18n.locale.to_s => item.content_translations }
-        item.published_translations = { I18n.locale.to_s => item.published_translations }
-        item.save!
+        @title_root_translation     = item.title_translations
+        @content_root_translation   = item.content_translations
+        @published_root_translation = item.published_translations
       end
+
+      item.title_translations     = Hash[@locales.collect { |locale| [locale, @title_root_translation] }]
+      item.content_translations   = Hash[@locales.collect { |locale| [locale, @content_root_translation] }]
+      item.published_translations = Hash[@locales.collect { |locale| [locale, @published_root_translation] }]
+      item.save!
     end
   end
 
