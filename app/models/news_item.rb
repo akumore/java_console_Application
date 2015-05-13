101358e91b5cce41d1c748596de95443a822c701
Phil Schilter <phil.schilter@gmail.com>
2015-05-13 15:18:31 +0200

    define my own getter and setter for published field

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -11,7 +11,7 @@ class NewsItem
     embeds_many :"documents_#{locale}", class_name: "NewsItemDocument", cascade_callbacks: true
   end
 
-  scope :published, lambda { where(published: true) }
+  scope :published, lambda { where '$or' => [ { published: true }, { published: 'true' }, { published: '1' } ] }
 
   PER_PAGE = 6
 
@@ -35,5 +35,13 @@ class NewsItem
     define_method "#{attrib}_in_locale" do |locale|
       I18n.with_locale(locale) { send attrib.to_sym }
     end
+  end
+
+  def published=(state)
+    self.write_attribute :published, ['1', true, 'true'].include?(state)
+  end
+
+  def published
+    ['1', true, 'true'].include? self.read_attribute(:published)[I18n.locale.to_s]
   end
 end
