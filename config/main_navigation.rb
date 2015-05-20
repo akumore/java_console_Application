218f2095c8e6625aa92187ebfa57e9480dce69b2
Phil Schilter <phil.schilter@gmail.com>
2015-05-20 13:41:52 +0200

    fix styling of navigation

--- config/main_navigation.rb
+++ config/main_navigation.rb
@@ -40,7 +40,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :jobs, t('navigation.main.jobs'), t('jobs_url'), highlights_on: proc { controller.request.path == I18n.t('jobs_url') } do |jobs|
       jobs_subnavigation.each do |page|
-        jobs.item page.id, page.title, "/#{I18n.locale}/#{page.name}"
+        jobs.item page.id, page.title, "/#{I18n.locale}/#{page.name}", highlights_on: proc { controller.request.path == "/#{I18n.locale}/#{page.name}" }
       end
 
       jobs.dom_class = 'clearfix'
@@ -48,7 +48,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :company, t('navigation.main.company'), t('company_url'), highlights_on: proc { controller.request.path == I18n.t('company_url') } do |company|
       company_subnavigation.each do |page|
-        company.item page.id, page.title, "/#{I18n.locale}/#{page.name}"
+        company.item page.id, page.title, "/#{I18n.locale}/#{page.name}", highlights_on: proc { controller.request.path == "/#{I18n.locale}/#{page.name}" }
       end
 
       company.dom_class = 'clearfix'
