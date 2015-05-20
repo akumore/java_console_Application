4d3297ee5a8505e9f3583bec113d239eb25ca17a
Phil Schilter <phil.schilter@gmail.com>
2015-05-20 14:29:15 +0200

    update specs

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -6,16 +6,16 @@ describe 'Language Navigation' do
 
   %w(de en fr it).each do |lang|
     it "links to the language version #{lang.upcase}" do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.meta-navigation' do
-        page.should have_link lang, :href => root_path(:locale => lang)
+        expect(page).to have_link lang, href: root_path(locale: lang)
       end
     end
 
     it "links to contacts for #{lang.upcase}" do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.meta-navigation' do
-        page.should have_link I18n.t('navigation.meta.contact'), :href => contact_path(:locale => lang)
+        expect(page).to have_link I18n.t('navigation.meta.contact'), href: contact_path(locale: lang)
       end
     end
   end
@@ -27,67 +27,65 @@ describe "Main Navigation" do
 
   shared_examples_for "All language versions" do |lang|
     it 'links to the list of real estates' do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.real_estate'), :href => real_estates_path(:locale => lang)
+        expect(page).to have_link I18n.t('navigation.main.real_estate'), href: real_estates_path(locale: lang)
       end
     end
 
     describe "offer subnavigation" do
       it "links to the offers for rent/commercial" do
-        visit root_path(:locale => lang)
+        visit root_path(locale: lang)
         within all('.sub-navigation')[0] do
-          page.should have_link I18n.t('real_estates.search_filter.for_rent'), :href => "/#{lang}/real_estates?offer=for_rent&utilization=commercial"
+          expect(page).to have_link I18n.t('real_estates.search_filter.for_rent'), href: "/#{lang}/real_estates?offer=for_rent&utilization=commercial"
         end
       end
 
       it "links to the offers for sale/private" do
-        visit root_path(:locale => lang)
+        visit root_path(locale: lang)
         within all('.sub-navigation')[0] do
-          page.should have_link I18n.t('real_estates.search_filter.for_sale'), :href => "/#{lang}/real_estates?offer=for_sale&utilization=private"
+          expect(page).to have_link I18n.t('real_estates.search_filter.for_sale'), href: "/#{lang}/real_estates?offer=for_sale&utilization=private"
         end
       end
     end
 
     it "links to the content page 'jobs'" do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.jobs'), :href => "/#{lang}/jobs"
+        expect(page).to have_link I18n.t('navigation.main.jobs'), href: "/#{lang}/jobs"
       end
     end
 
-    it "links to the subchapters of the content page 'jobs'" do
-      Page.create(:title => 'Jobs', :name => 'jobs', :locale => I18n.locale) do |jobs_page|
-        jobs_page.bricks << Brick::Title.new(:title => 'Brick-Title 1')
-        jobs_page.bricks << Brick::Title.new(:title => 'Brick-Title 2')
-      end
+    it "links to the sub pages of the content page 'jobs'" do
+      jobs_page = Page.create(title: 'Jobs', name: 'jobs', locale: I18n.locale)
+      Page.create(title: 'Jobs Subpage', name: 'jobs_subpage', locale: I18n.locale, parent_id: jobs_page.id)
 
       visit I18n.t('jobs_url')
 
       # scope within second sub-navigation
       within all('.sub-navigation')[1] do
-        page.should have_link 'Brick-Title 2', :href => "#{I18n.t('jobs_url')}#brick-title-2"
+        expect(page).to have_link('Jobs Subpage', href: "/#{I18n.locale}/jobs_subpage")
       end
     end
 
     it "links to the content page 'company'" do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.company'), :href => "/#{lang}/company"
+        expect(page).to have_link I18n.t('navigation.main.company'), href: "/#{lang}/company"
       end
     end
 
     it "links to the news" do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.news'), :href => news_items_path(:locale => lang)
+        expect(page).to have_link I18n.t('navigation.main.news'), href: news_items_path(locale: lang)
       end
     end
 
     it "doesn't show links to the reference projects" do
-      visit root_path(:locale => lang)
+      visit root_path(locale: lang)
       within '.main-navigation' do
-        page.should have_link I18n.t('navigation.main.reference_projects'), :href => reference_projects_path(:locale => lang)
+        expect(page).to have_link I18n.t('navigation.main.reference_projects'), href: reference_projects_path(locale: lang)
       end
     end
   end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -7,12 +7,12 @@ describe Page do
     end
 
     it 'does not pass validations' do
-      @page.should_not be_valid
+      expect(@page).to_not be_valid
     end
 
     it 'has 4 errors' do
       @page.valid?
-      @page.errors.should have(4).items
+      expect(@page.errors).to have(4).items
     end
   end
 
@@ -22,40 +22,39 @@ describe Page do
     end
 
     it 'passes validations' do
-      @page.should be_valid
+      expect(@page).to be_valid
     end
   end
 
   describe 'saving without a unique name' do
     it 'raises an error' do
-      Fabricate(:page, :name => 'uniquename')
-      expect { Fabricate(:page, :name => 'uniquename') }.to raise_error
+      Fabricate(:page, name: 'uniquename')
+      expect { Fabricate(:page, name: 'uniquename') }.to raise_error
     end
   end
 
   describe '.jobs_page' do
     it 'finds the jobs page' do
-      jobs_page = Fabricate(:page, :name => 'jobs', :locale => I18n.locale)
-      Page.jobs_page.should == jobs_page
+      jobs_page = Fabricate(:page, name: 'jobs', locale: I18n.locale)
+      expect(Page.jobs_page).to eq(jobs_page)
     end
   end
 
   describe '.company_page' do
     it 'finds the company page' do
-      company_page = Fabricate(:page, :name => 'company', :locale => I18n.locale)
-      Page.company_page.should == company_page
+      company_page = Fabricate(:page, name: 'company', locale: I18n.locale)
+      expect(Page.company_page).to eq(company_page)
     end
   end
 
   describe '.subnavigation' do
-    it 'returns only secondary titles' do
-      page = Page.create(:name => 'dummy', :title => 'Dummy', :locale => I18n.locale) do |page|
-        page.bricks << Brick::Title.new(:title => 'Test')
-        page.bricks << Brick::Text.new(:text => 'Hi there')
-        page.bricks << Brick::Title.new(:title => 'Test2')
-      end
+    it 'returns children of page' do
+      page = Page.create(name: 'dummy', title: 'Dummy', locale: I18n.locale)
+      Page.create(title: 'Sub Page 1', name: 'sub_page_1', locale: I18n.locale, parent_id: page.id)
+      Page.create(title: 'Sub Page 2', name: 'sub_page_2', locale: I18n.locale, parent_id: page.id)
+      Page.create(title: 'Sub Page 3', name: 'sub_page_3', locale: I18n.locale, parent_id: page.id)
 
-      page.subnavigation.count.should be(1)
+      expect(page.subnavigation.count).to eq(3)
     end
   end
 end
